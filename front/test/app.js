5dd3eb02af1d5531fce619a59e06053048f6d940
louise <bulone.louise@gmail.com>
2020-06-16 18:18:29 +0200

    feat(front): alternative routes

--- front/test/app.js
+++ front/test/app.js
@@ -18,43 +18,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
 // Calcul de la route
 // request = localhost:5000/route/v1/driving/{coordinates}?option=value&option=value
 // ATTENTION inversé coordinates = long,lat;long,lat
-var url = "http://localhost:5000/route/v1/driving/"+A[1]+','+A[0]+';'+B[1]+','+B[0]+'?overview=full&geometries=geojson';
-var request = new XMLHttpRequest();
+function getJSON(url,callback){
+    var request = new XMLHttpRequest();
 
-request.onload = function() {
-    if ( this.status == 200) {
-        var myJson = JSON.parse(this.responseText);
-        myFunction(myJson);
-    }
-};
-request.open("GET", url, true);
-request.send();
+    request.onload = function() {
+        if ( this.status == 200) {
+            var myJson = JSON.parse(this.responseText);
+            callback(myJson);
+        }
+    };
+    request.open("GET", url, true);
+    request.send();
+}
 
-function myFunction(json) {
+
+var urlRoute = "http://localhost:5000/route/v1/driving/"+A[1]+','+A[0]+';'+B[1]+','+B[0]+'?overview=full&geometries=geojson';
+var affichageRoute = function(json) {
     console.log("GET");
+
     // Affichage d'info complémentaire
     var infoRoute = document.getElementById("route");
     var text = json.waypoints[0].name;
     text +='\n\tdistance : '+json.routes[0].distance;
     text +='\n\tduration : '+json.routes[0].duration;
     infoRoute.innerText=text;
-/*
-    var coords = [];
-    var i, n=json.routes[0].geometry.coordinates.length;
-    for (i = 0; i < n; i++) {        
-        const coord = json.routes[0].geometry.coordinates[i];
-        coords.push([coord[1],coord[0]]);
-    }
-
-    var lineString = {
-        "type": "LineString",
-        "coordinates": coords
-    };
-*/
 
     // Affichage du chemin sur la map
     var myStyle = {
-        "color": "#ff7800",
+        "color": "#0078ff",
         "weight": 5
     };
 
@@ -62,3 +53,34 @@ function myFunction(json) {
     myLayer.addTo(map);
 };
 
+getJSON(urlRoute,affichageRoute);
+
+
+var urlRoutes = "http://localhost:5000/route/v1/driving/"+A[1]+','+A[0]+';'+B[1]+','+B[0]+'?alternatives=3&overview=full&geometries=geojson';
+var affichageRoutes = function(json) {
+    console.log("GET");
+
+    // Affichage d'info complémentaire
+    var infoRoute = document.getElementById("route");
+    var text = json.waypoints[0].name;
+    text +='\n\tdistance0 : '+json.routes[0].distance;
+    text +='\n\tduration0 : '+json.routes[0].duration;
+    infoRoute.innerText=text;
+
+    // Affichage du chemin sur la map
+    var myStyle = {
+        "color": "#0078ff",
+        "weight": 5,
+        "opacity": 0.5
+    };
+
+    var n = json.routes.length;
+    console.log("alternatives : "+n)
+    for (let i = 0; i < n; i++) {
+        //const element = array[i];
+        var myLayer = L.geoJSON(json.routes[i].geometry,{style: myStyle});
+        myLayer.addTo(map);            
+    }
+};
+
+getJSON(urlRoutes,affichageRoutes);
