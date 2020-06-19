ccf2b2d38947923009eaf79cbcb28e715097a6af
Augustin Gjini <augustin.gjini@pm.me>
2020-06-19 12:16:41 +0200

    add front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { LatLngLiteral } from "leaflet";
 
-import { StyleSheet, Text, View } from 'react-native';
+function OurMap({start, end}: { start: LatLngLiteral, end: LatLngLiteral }) {
 
-function App() {
+  const zoom = 11;
+  const center = {
+    lat: (start.lat + end.lat) / 2,
+    lng: (start.lng + end.lng) / 2
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Map className="map" center={center} zoom={zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>A pretty CSS3 popup.<br/>Easily customizable.</Popup>
+      </Marker>
+    </Map>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+function App() {
+  const start = {lat: 44.5180226, lng: 3.4991057};
+  const end = {lat: 44.31901305, lng: 3.57802065202088};
+  return (
+    <div className="App">
+      <OurMap start={start} end={end}/>
+    </div>
+  );
+}
 
-registerRootComponent(App);
+export default App;
