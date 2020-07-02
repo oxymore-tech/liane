091b38006628ef3a5fd7ee11ff4ff8ca91ca8515
Augustin Gjini <augustin.gjini@pm.me>
2020-07-02 13:45:23 +0200

    fix(deploy)

--- deploy/prepare-data.sh
+++ deploy/prepare-data.sh
@@ -2,28 +2,30 @@
 
 BASEDIR=$(dirname "$0")
 
-sudo mkdir -p /data/liane/osm
+OSM_DATA=/data/liane/osm
+
+sudo mkdir -p ${OSM_DATA}
 
 function download() {
-    wget -P data -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
-    wget -P data -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
+    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
+    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
 }
 
 function merge() {
-    if [ ! -f data/all.osm.pbf ]
+    if [ ! -f ${OSM_DATA}/all.osm.pbf ]
     then
-        docker run -t -v "/data/liane/osm:/data" yagajs/osmosis osmosis --read-pbf /data/midi-pyrenees-latest.osm.pbf --read-pbf /data/languedoc-roussillon-latest.osm.pbf --merge --write-pbf /data/all.osm.pbf
+        docker run -t -v "${OSM_DATA}:/data" yagajs/osmosis osmosis --read-pbf /data/midi-pyrenees-latest.osm.pbf --read-pbf /data/languedoc-roussillon-latest.osm.pbf --merge --write-pbf /data/all.osm.pbf
     fi;
 }
 
 function prepare() {
     local country=$1
 
-    docker run -t -v "/data/liane/osm:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
+    docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
 
-    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
-    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
-    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-customize /data/${country}.osrm
+    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
+    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
+    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-customize /data/${country}.osrm
 }
 
 download
