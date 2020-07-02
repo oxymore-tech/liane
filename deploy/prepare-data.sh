6248077cdd4014b9c0e3f0a204003b528a9c0770
Augustin Gjini <augustin.gjini@pm.me>
2020-07-02 13:42:42 +0200

    fix(deploy)

--- deploy/prepare-data.sh
+++ deploy/prepare-data.sh
@@ -2,6 +2,8 @@
 
 BASEDIR=$(dirname "$0")
 
+mkdir -p /data/liane/osm
+
 function download() {
     wget -P data -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
     wget -P data -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
@@ -10,18 +12,18 @@ function download() {
 function merge() {
     if [ ! -f data/all.osm.pbf ]
     then
-        docker run -t -v "${BASEDIR}/data:/data" yagajs/osmosis osmosis --read-pbf /data/midi-pyrenees-latest.osm.pbf --read-pbf /data/languedoc-roussillon-latest.osm.pbf --merge --write-pbf /data/all.osm.pbf
+        docker run -t -v "/data/liane/osm:/data" yagajs/osmosis osmosis --read-pbf /data/midi-pyrenees-latest.osm.pbf --read-pbf /data/languedoc-roussillon-latest.osm.pbf --merge --write-pbf /data/all.osm.pbf
     fi;
 }
 
 function prepare() {
     local country=$1
 
-    docker run -t -v "${BASEDIR}/data:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
+    docker run -t -v "/data/liane/osm:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
 
-    docker run -t -v "${BASEDIR}/data:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
-    docker run -t -v "${BASEDIR}/data:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
-    docker run -t -v "${BASEDIR}/data:/data" osrm/osrm-backend osrm-customize /data/${country}.osrm
+    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
+    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
+    docker run -t -v "/data/liane/osm:/data" osrm/osrm-backend osrm-customize /data/${country}.osrm
 }
 
 download
