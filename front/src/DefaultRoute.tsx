8c16cfa5e46101c3e13a3b8bec2f54c858aab5a9
louise <bulone.louise@gmail.com>
2020-07-20 16:18:47 +0200

    fix(front): selector for each point

--- front/src/DefaultRoute.tsx
+++ front/src/DefaultRoute.tsx
@@ -8,8 +8,6 @@ import {Marker} from "react-leaflet";
 import {PointsOverlay} from "./PointsOverlay";
 import {Point} from "./Point";
 import {addressService} from "./api/address-service";
-import {Address} from "./api/address";
-import VirtualizedSelect from "react-virtualized-select";
 
 export interface Points {
     readonly waypoints: Point[];
@@ -45,11 +43,6 @@ export function DefaultRouteComponent({start, end}: { start: Point, end: Point }
 
     const [lastAddressName, setLastAddressName] = useState(start.address);
 
-    const [selectedAddress, setSelectedAddress] = useState<Address>({
-        displayName: "default",
-        coordinate: start.coordinate
-    });
-    const [addresses, setAddresses] = useState<Address[]>([]);
 
     const [route, setRoute] = useState<Route>();
 
@@ -100,6 +93,7 @@ export function DefaultRouteComponent({start, end}: { start: Point, end: Point }
         }
     }, [lastAddressName]);
 
+
     function setWaypoint(i: number, p: Point) {
         setWaypoints(waypoints.map((w, wi) => {
             if (wi === i) {
@@ -111,29 +105,7 @@ export function DefaultRouteComponent({start, end}: { start: Point, end: Point }
 
     // Option<TValue> | Options<TValue> | null
     // OnChangeHandler<OptionValues, Option<OptionValues> | Options<OptionValues>>
-    const onChange = (sa: Address | null) => {
-        if (sa != null) {
-            setSelectedAddress(selectedAddress);
-        } else {
-            setSelectedAddress({
-                coordinate: start.coordinate,
-                displayName: start.address
-            })
-        }
-    }
-    
-    function loadAddresses(displayName: string) {
-        return addressService.Search(displayName)
-            .then((a) => {
-                setAddresses(a);
-                return {options:new Array(a)};
-            })
-    }
-    
 
-    function onSelectClick() {
-// equals to previous selectClick in point
-    }
 
     let routeOverlay = defaultRouteOverlay(waypoints[0].coordinate, waypoints[1].coordinate, route);
 
@@ -145,25 +117,8 @@ export function DefaultRouteComponent({start, end}: { start: Point, end: Point }
         <PointsOverlay waypoints={waypoints} onChange={setWaypoint} onSelect={setSelectedPoint}
                        onInput={setLastAddressName}>
             <div>
-                <VirtualizedSelect
-                    async
-                    backspaceRemoves={false}
-                    labelKey='displayName' 
-                    
-                    loadOptions={loadAddresses}
-                    options={new Array(addresses)}
-                    
-                    /*onChange={selectedOption => {
-                        if (selectedOption?.hasOwnProperty("coordinate") && selectedOption?.hasOwnProperty("displayName")) {
-                            onChange({coordinate: selectedOption?.coordinate, displayName: selectedOption?.displayName});
-                        }
-                    }
-                    }*/
-                    
-                    onValueClick={onSelectClick}
-                    value={selectedAddress}
-                    valueKey='displayName'
-                />
+
+
             </div>
         </PointsOverlay>
     </>;
--- front/src/Point.tsx
+++ front/src/Point.tsx
@@ -1,5 +1,8 @@
 import {LatLngLiteral} from "leaflet";
-import React, {ChangeEvent, InputHTMLAttributes, useState} from "react";
+import React, {ChangeEvent, InputHTMLAttributes, useEffect, useState} from "react";
+import VirtualizedSelect from "react-virtualized-select";
+import {Address} from "./api/address";
+import {addressService} from "./api/address-service";
 
 export interface Point {
     readonly coordinate: LatLngLiteral;
@@ -18,6 +21,23 @@ export interface PointComponentProps {
 
 export function PointComponent({index, point, optional, onChange, onSelect, onInput}: PointComponentProps) {
 
+    const [addresses, setAddresses] = useState<Address[]>([]);
+    const [input, setInput] = useState("");
+
+    const [selectedAddress, setSelectedAddress] = useState<Address>({
+        displayName: point.address,
+        coordinate: point.coordinate
+    });
+
+    useEffect(() => {
+        if (input) {
+            addressService.Search(input)
+                .then(a => setAddresses(a));
+        } else {
+            setAddresses([]);
+        }
+    }, [input])
+
     function excludeClick() {
         onChange(index, {...point, exclude: !point.exclude});
     }
@@ -28,15 +48,46 @@ export function PointComponent({index, point, optional, onChange, onSelect, onIn
 
     function inputChange(event: ChangeEvent<HTMLInputElement>) {
         // TODO: debounce, filtre les events et merge en seul, checker librairies eg. lodash
-        
+
         onInput(event.target.value);
     }
 
+    useEffect(() => {
+        onChange(index, {...point, coordinate: selectedAddress.coordinate, address: selectedAddress.displayName})
+    }, [selectedAddress]);
+
     return <>
-        
-        <input type="text" value={point.address} onClick={selectClick} onChange={inputChange}/>
+
         {
             optional ? <button onClick={excludeClick}> {point.exclude} </button> : null
+//                <input type="text" value={point.address} onClick={selectClick} onChange={inputChange}/>
+
         }
+
+        <VirtualizedSelect
+
+            backspaceRemoves={false}
+
+            labelKey='displayName'
+
+            options={addresses}
+
+            onInputChange={input => {
+                setInput(input);
+                return input;
+            }}
+
+            onValueClick={selectClick}
+            value={selectedAddress}
+
+            onChange={option => {
+                if (option) {
+                    setSelectedAddress((option.valueOf() as Address));
+                }
+            }}
+
+
+        />
+
     </>;
 }
