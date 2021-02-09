9163115b61c04204f472007c1120db5f294ee699
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-02-09 11:55:39 +0100

    test location in bg

--- mobile/liane-app/App.tsx
+++ mobile/liane-app/App.tsx
@@ -1,21 +1,49 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
+import * as BackgroundFetch from "expo-background-fetch"
+import * as TaskManager from "expo-task-manager"
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    return;
+  }
+  if (data) {
+    console.log('LOCATION : ', data);
+    const { locations } = data;
+    // do something with the locations captured in the background
+  }
+});
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  
+  /*
+  (async () => {
+    try {
+      await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+        minimumInterval: 5, // seconds,
+      })
+      console.log("Task registered")
+    } catch (err) {
+      console.log("Task Register failed:", err)
+    }
+  })();
+  */
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+      if (status === 'granted') {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          distanceInterval : 1,
+          timeInterval : 10
+        });
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
     })();
   }, []);
 
