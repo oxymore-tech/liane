5d0892de867b469e0043786c972d110501ff82b5
fanfois <francois.deligand@gmail.com>
2021-02-09 12:28:19 +0100

    web + blabla start

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+import { Blabla } from '../components/blabla';
+
+
+export default function Home() {
+  return <Blabla text="blabla"/>;
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: [
+      "./src/pages/**/*.tsx",
+      "./src/components/**/*.tsx"
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
