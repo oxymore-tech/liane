864559a2335e68fa01696d452af79366581085dd
fanfois <francois.deligand@gmail.com>
2021-02-09 14:16:49 +0100

    div ok

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,9 +4,7 @@ import dynamic from "next/dynamic";
 const Map = dynamic(() => import("../components/map"), {ssr: false});
 
 export default function Home() {
-  return 
-    <div>
-      <Blabla text="blabla"/>
-      <Map className="w-96 h-96"/>
-    </div>;
+  return <div>
+    <Map className="w-full h-screen"/>
+  </div>;
 }
