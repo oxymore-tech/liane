98552be1f802704d0aa28cd365207379bff82332
fanfois <francois.deligand@gmail.com>
2021-02-09 12:53:48 +0100

    map

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import { Blabla } from '../components/blabla';
-
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("../components/map"), {ssr: false});
 
 export default function Home() {
-  return <Blabla text="blabla"/>;
+  return 
+    <div>
+      <Blabla text="blabla"/>
+      <Map className="w-96 h-96"/>
+    </div>;
 }
