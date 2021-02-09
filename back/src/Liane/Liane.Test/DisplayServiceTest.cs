3ad93cee83f921b44e730d43915b68c72639c0ff
fanfois <francois.deligand@gmail.com>
2021-02-09 10:21:18 +0100

    clean

--- back/src/Liane/Liane.Test/DisplayServiceTest.cs
+++ back/src/Liane/Liane.Test/DisplayServiceTest.cs
@@ -1,14 +1,6 @@
-using System.Collections.Immutable;
-using System.Linq;
 using System.Threading.Tasks;
 using Liane.Api;
-using Liane.Api.Routing;
 using Liane.Service.Internal;
-using Liane.Service.Internal.Matching;
-using Liane.Service.Internal.Osrm;
-using Liane.Service.Internal.Osrm.Response;
-using Liane.Test.Util;
-using Moq;
 using NUnit.Framework;
 
 namespace Liane.Test
