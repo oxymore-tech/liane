8b6813746e4d9886ee552ddf40a73a91ad8be5a9
Augustin Gjini <augustin.gjini@pm.me>
2021-02-10 14:20:08 +0100

    wip display service

--- back/src/Liane/Liane.Service/Internal/DisplayServiceImpl.cs
+++ back/src/Liane/Liane.Service/Internal/DisplayServiceImpl.cs
@@ -16,14 +16,15 @@ namespace Liane.Service.Internal
 
         public async Task<LabeledPosition> SnapPosition(LatLng position)
         {
-            var rallyingPoints = ImmutableList.Create(new LabeledPosition("Blajoux-Parking", new LatLng(44.33719040451529, 3.4833812113191227)));
-            var redis = ConnectionMultiplexer.Connect("localhost");
+            // var rallyingPoints = ImmutableList.Create(new LabeledPosition("Blajoux-Parking", new LatLng(44.33719040451529, 3.4833812113191227)));
+            var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
             var database = redis.GetDatabase();
             var redisKey = new RedisKey("rallying points");
             await database.GeoAddAsync(redisKey, 3.4833812113191227, 44.33719040451529, new RedisValue("Blajoux-Parking"));
             var results = await database.GeoRadiusAsync(redisKey, position.Lng, position.Lat, 500);
-            return results[0];
+            var geoRadiusResult = results[0];
+            var geoPosition = geoRadiusResult.Position!.Value;
+            return new LabeledPosition(geoRadiusResult.Member, new LatLng(geoPosition.Latitude, geoPosition.Longitude));
         }
-
     }
 }
