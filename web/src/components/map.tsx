55eab358369cd0a47916dcf25ea56d8ba4b1cc37
ErwanGauduchon <erwan.gauduchon@outlook.fr>
2021-02-16 12:53:11 +0100

    trips

--- web/src/components/map.tsx
+++ web/src/components/map.tsx
@@ -19,21 +19,22 @@ const customIcon = icon({
 });
 
 export function printTrips(start: RallyingPoint) {
-  var routes = [];
+  let routes = [];
   displayService.ListTripsFrom(start.id, start.position.lat, start.position.lng).then(
     trips => {
       trips.forEach(trip => {
-          var route = [];
+          let route = [];
           trip.coordinates.forEach(point => {
               route.push([point.position.lat, point.position.lng]);
             }
           );
-          routes.push(<Polyline positions={route}/>)
+          routes.push(<Polyline positions={route}/>);
         }
       );
     }
   );
   return routes;
+  //return [<Polyline positions={[[44.5, 3.5], [44.4, 3.6]]}/>, <Polyline positions={[[44, 3], [45, 4]]}/>];
 }
 
 function Map({className, center, start}: MapProps) {
@@ -59,7 +60,7 @@ function Map({className, center, start}: MapProps) {
     }
     {
       start &&
-      <div>{printTrips(start)}</div>
+      <div> { printTrips(start) } </div>
     }
     <Polyline positions={[[44.5180226, 3.4991057], [44.38624954223633, 3.6189568042755127], [44.31901305, 3.57802065202088]]} 
               color={"#ff0000"} 
