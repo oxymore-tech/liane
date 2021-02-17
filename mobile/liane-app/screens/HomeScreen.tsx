7b4790b6d30097d2e144921131799c88c7c7c0a9
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-02-17 10:03:07 +0100

    maj tailwind

--- mobile/liane-app/screens/HomeScreen.tsx
+++ mobile/liane-app/screens/HomeScreen.tsx
@@ -1,15 +1,28 @@
 import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, SafeAreaView } from 'react-native';
 import { AuthContext } from '../utils/authContext';
+
+// Style
+import tailwind from 'tailwind-rn';
 
 const HomeScreen = ({ route, navigation } : any) => {
     const { SignOutButton } = route.params;
     return (
-        <View>
-            <Text>Ceci est la page principale - Welcome !</Text>
-            <Button title="Menu" onPress={() => navigation.openDrawer()} />
-            <SignOutButton/>
-        </View>
+        <><SafeAreaView style={tailwind('h-full')}>
+            <View style={tailwind('pt-12 items-center')}>
+                <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+                    <Text style={tailwind('text-blue-800 font-semibold')}>
+                        Hello Tailwind
+                    </Text>
+                </View>
+            </View>
+            <View>
+                <Text>Ceci est la page principale - Welcome !</Text>
+                <Button title="Menu" onPress={() => navigation.openDrawer()} />
+                <SignOutButton />
+            </View>
+        </SafeAreaView>
+        </>
     );
 };
 
