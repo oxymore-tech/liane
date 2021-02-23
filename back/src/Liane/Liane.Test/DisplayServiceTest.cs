f6e59668a5407da08871164ff964684f0a772fca
Karim_Elhadji <karimelhadji21@gmail.com>
2021-02-23 14:14:10 +0100

    tests sur obtention des clés + filtrage ok

--- back/src/Liane/Liane.Test/DisplayServiceTest.cs
+++ back/src/Liane/Liane.Test/DisplayServiceTest.cs
@@ -139,27 +139,62 @@ namespace Liane.Test
 
         [Test]
         [Category("Integration")]
-        public async Task EdgeKeys()
+        public async Task EdgeKeys() // 
         {
-            await SetUpRedisAsync();
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-            IServer server = redis.GetServer("localhost", 8081);
+            ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+            var endPoints = redis.GetEndPoints();
+            Console.WriteLine($"\n \n ENDPOINTS : {endPoints.Count()}");
+            IServer server = redis.GetServer(endPoints[0]);
             var actual = displayService!.EdgeKeys(server);
-            var expected = ImmutableList.Create("FRANCOIS CHUT");
-            actual.WithDeepEqual(expected);
+            Console.WriteLine($"\n \n ACTUAL : {Print.ImmutableListToString(actual)}");
+            var expected = ImmutableHashSet.Create(new RedisKey("Blajoux_Parking|Montbrun_En_Bas|Monday|8"), 
+                                                new RedisKey("Blajoux_Parking|Montbrun_En_Bas|Tuesday|8"),
+                                                new RedisKey("Blajoux_Parking|Montbrun_En_Bas|Wednesday|8"), 
+                                                new RedisKey("Montbrun_En_Bas|La_Malene_Parking|Wednesday|9"),
+                                                new RedisKey("La_Malene_Parking|Severac_dAveyron_Rond_Point|Wednesday|10"),
+                                                new RedisKey("LesBondons_Parking|Prades|Monday|8"), 
+                                                new RedisKey("LesBondons_Parking|Prades|Tuesday|8"), 
+                                                new RedisKey("LesBondons_Parking|Prades|Thursday|8"), 
+                                                new RedisKey("Prades|La_Malene_Parking|Wednesday|9"), 
+                                                new RedisKey("Rouffiac_Boulangerie|SaintEnimie_Parking|Monday|8"), 
+                                                new RedisKey("Rouffiac_Boulangerie|SaintEnimie_Parking|Tuesday|8"), 
+                                                new RedisKey("Rouffiac_Boulangerie|SaintEnimie_Parking|Friday|8"), 
+                                                new RedisKey("SaintEnimie_Parking|Mende|Monday|10"),
+                                                new RedisKey("SaintEnimie_Parking|Mende|Tuesday|10"),
+                                                new RedisKey("Rodez_Mac_Drive|Lanuejols_Parking_Eglise|Saturday|8"));
+            actual.WithDeepEqual(expected)
+                .Assert();
         }
 
         [Test]
         [Category("Integration")]
-        public async Task FilterByDay()
+        public async Task FilterBySaturday()
         {
-            await SetUpRedisAsync();
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-            IServer server = redis.GetServer("localhost", 8081);
+            var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+            var endPoints = redis.GetEndPoints();
+            IServer server = redis.GetServer(endPoints[0]);
             var edgeKeys = displayService!.EdgeKeys(server);
-            var actual = displayService!.FilterByDay(edgeKeys, "samedi");
-            var expected = ImmutableList.Create("KARIMOU");
-            actual.WithDeepEqual(expected);
+            var actual = displayService!.FilterByDay(edgeKeys, "Saturday");
+            var expected = ImmutableList.Create(new RedisKey("Rodez_Mac_Drive|Lanuejols_Parking_Eglise|Saturday|8")); // 
+            actual.WithDeepEqual(expected)
+                .Assert();
+        }
+
+        [Test]
+        [Category("Integration")]
+        public async Task FilterByMonday()
+        {
+            var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+            var endPoints = redis.GetEndPoints();
+            IServer server = redis.GetServer(endPoints[0]);
+            var edgeKeys = displayService!.EdgeKeys(server);
+            var actual = displayService!.FilterByDay(edgeKeys, "Monday");
+            var expected = ImmutableHashSet.Create(new RedisKey("Blajoux_Parking|Montbrun_En_Bas|Monday|8"),
+                                                   new RedisKey("LesBondons_Parking|Prades|Monday|8"),
+                                                   new RedisKey("Rouffiac_Boulangerie|SaintEnimie_Parking|Monday|8"),
+                                                   new RedisKey("SaintEnimie_Parking|Mende|Monday|10"));
+            actual.WithDeepEqual(expected)
+                .Assert();
         }
 
         [Test]
