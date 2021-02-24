f5cd747a31fa9be5e7d6671876da14846d70f127
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-02-24 17:00:14 +0100

    maj map - ajout select (depart/arrivee)

---
+++ web/assets/time.data.ts
@@ -0,0 +1,38 @@
+const days = [
+    { label: "Lundi", value: 1 },
+    { label: "Mardi", value: 2 },
+    { label: "Mercredi", value: 3 },
+    { label: "Jeudi", value: 4 },
+    { label: "Vendredi", value: 5 },
+    { label: "Samedi", value: 6 },
+    { label: "Dimanche", value: 0 },
+];
+
+const hours = [
+    { label: "0h", value: 0 },
+    { label: "1h", value: 1 },
+    { label: "2h", value: 2 },
+    { label: "3h", value: 3 },
+    { label: "4h", value: 4 },
+    { label: "5h", value: 5 },
+    { label: "6h", value: 6 },
+    { label: "7h", value: 7 },
+    { label: "8h", value: 8 },
+    { label: "9h", value: 9 },
+    { label: "10h", value: 10 },
+    { label: "11h", value: 11 },
+    { label: "12h", value: 12 },
+    { label: "13h", value: 13 },
+    { label: "14h", value: 14 },
+    { label: "15h", value: 15 },
+    { label: "16h", value: 16 },
+    { label: "17h", value: 17 },
+    { label: "18h", value: 18 },
+    { label: "19h", value: 19 },
+    { label: "20h", value: 20 },
+    { label: "21h", value: 21 },
+    { label: "22h", value: 22 },
+    { label: "23h", value: 23 },
+  ]
+
+export { days, hours }
