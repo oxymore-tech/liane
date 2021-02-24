915d396c39eccf3a8ef606d2f6aa4730778ff427
fanfois <64696654+fanfois@users.noreply.github.com>
2021-02-24 11:50:26 +0100

    Feature/display6 (#17)
    * avant plan smelbe réglé avec map
    
    * avant plan smelbe réglé avec map
    
    * supp slider
    
    * mise en place des Select front
    
    * branche à jour
    
    * moins de warnings
    
    * correction warning
    
    * élagage du code
    
    * merge à faire
    
    Co-authored-by: Karim_Elhadji <karimelhadji21@gmail.com>

--- web/src/components/map.tsx
+++ web/src/components/map.tsx
@@ -1,10 +1,10 @@
-import React, { memo, useEffect, useRef, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer, Polyline } from "react-leaflet";
-import { icon, LatLngExpression, marker} from "leaflet";
+import { icon, LatLngExpression} from "leaflet";
 import { RallyingPoint, LatLng, Trip} from "../api";
 import { displayService } from "../api/display-service";
-import { Console } from "console";
+import Select from "react-select";
 
 interface MapProps {
   className?: string;
@@ -35,25 +35,42 @@ const customIconRed = icon({
 
 const MemoPolyline = memo(Polyline);
 
-/*export function getRoutes(trips: Trip[], routesEdges: Map<string, LatLngExpression[][]>){
-  console.log("TRIPS HERE: ", trips);
-  console.log("ROUTESEDGES HERE: ", routesEdges);
-  let routes = [];
-  trips.forEach(trip => {
-    let route = [];
-    trip.coordinates.forEach(
-      (point, index)  => {
-        if (index != (trip.coordinates.length - 1)) {
-          let key = trip.coordinates[index].id + "_" + trip.coordinates[index + 1].id;
-          route.push(routesEdges[key]);
-        }
-      }
-    );
-    routes.push(<Polyline positions={route.flat()}/>);
-  });
-  console.log("ROUTES : ", routes);
-  return routes
-}*/
+const days = [
+  { label: "Lundi", value: "Monday" },
+  { label: "Mardi", value: "Tuesday" },
+  { label: "Mercredi", value: "Wednesday" },
+  { label: "Jeudi", value: "Thursday" },
+  { label: "Vendredi", value: "Friday" },
+  { label: "Samedi", value: "Saturday" },
+  { label: "Dimanche", value: "Sunday" },
+]
+
+const hours = [
+  { label: "0h", value: 0 },
+  { label: "1h", value: 1 },
+  { label: "2h", value: 2 },
+  { label: "3h", value: 3 },
+  { label: "4h", value: 4 },
+  { label: "5h", value: 5 },
+  { label: "6h", value: 6 },
+  { label: "7h", value: 7 },
+  { label: "8h", value: 8 },
+  { label: "9h", value: 9 },
+  { label: "10h", value: 10 },
+  { label: "11h", value: 11 },
+  { label: "12h", value: 12 },
+  { label: "13h", value: 13 },
+  { label: "14h", value: 14 },
+  { label: "15h", value: 15 },
+  { label: "16h", value: 16 },
+  { label: "17h", value: 17 },
+  { label: "18h", value: 18 },
+  { label: "19h", value: 19 },
+  { label: "20h", value: 20 },
+  { label: "21h", value: 21 },
+  { label: "22h", value: 22 },
+  { label: "23h", value: 23 },
+]
  
 export function getRoutes2(routesEdges: Map<string, LatLngExpression[][]>){
   let routes = [];
@@ -73,22 +90,10 @@ function  Map({className, center, start}: MapProps) {
     setMyStart(start);
   }, [start]);
 
-  /*useEffect(() => {
-    if (myStart != null) {
-      displayService.ListTripsFrom(myStart.id, myStart.position.lat, myStart.position.lng).then(
-        result => {setTrips(result)});
-    }
-  }, [myStart]);*/
-  //console.log("TRIPS : ", trips);
-
   useEffect(() => {
     if (myStart != null) {
       displayService.ListTripsFrom(myStart.id, myStart.position.lat, myStart.position.lng).then(
         result => {setTrips(result)});
-      /*if (trips != []) {
-        displayService.ListRoutesEdgesFrom(trips).then(
-            result => {setRoutes(getRoutes2(result))});
-        }*/
       displayService.ListDestinationsFrom(myStart.id, myStart.position.lat, myStart.position.lng).then(
         result => {setDestinations(result)});
       }
@@ -104,64 +109,95 @@ function  Map({className, center, start}: MapProps) {
       }
   }, [trips]);
 
-  return <MapContainer className={className} center={center}
-                       zoom={12}
-                       scrollWheelZoom={true}
-                       dragging={true}
-                       touchZoom={false}
-                       style={{zIndex: 2}}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      zIndex={2}
+  return  <div> 
+      <div className="container">
+      <div className="row" style={{top: 10, right: 10, width: 250, zIndex: 3, position : "absolute"}}>
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <Select options={ days } placeholder="Sélectionnez un jour"/>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="row" style={{top: 60, right: 10, width: 250, zIndex: 2, position : "absolute"}}>
+        Départ entre :
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <Select options={ hours } placeholder="Sélectionnez une heure"/>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="row" style={{top: 130, right: 10, width: 250, zIndex: 1, position : "absolute"}}>
+        et :
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <Select options={ hours } placeholder="Sélectionnez une heure"/>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+    </div>
+      <MapContainer className={className} center={center}
+                        zoom={12}
+                        scrollWheelZoom={true}
+                        dragging={true}
+                        touchZoom={false}
+                        style={{zIndex: 0, position : "relative"}}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        zIndex={2}
 
-    />
-    {
-      myStart && 
-      <Marker position={myStart.position} icon={customIcon}>
-        <Popup>
-          <h3>{myStart.id}</h3>
-        </Popup>
-      </Marker>
-    }
-    {
-      myStart &&
-      <div> 
-        {
-          routes.map((route, index) => (
-            <MemoPolyline key={index} positions={route}/>
-          ))
-        } 
-      </div>
-    }
-    {
-      myStart &&
-      <div>
-        {destinations.map((point, index) => (
-          <Marker key={index} position={point.position} icon={customIconGray} eventHandlers={{
-            click: () => {
-              setMyStart(point);
-                            
-            },
-          }}>
-          </Marker>
-        ))}
-      </div>
-    }
-    {
-      myStart &&
-      <div>
-        {steps.map((point, index) => (
-          <Marker key={index} position={point.position} icon={customIconRed} eventHandlers={{
-            click: () => {
-              setMyStart(point);
-                            
-            },
-          }}>
-          </Marker>
-        ))}
-      </div>
-    }
-  </MapContainer>;
+      />
+      {
+        myStart && 
+        <Marker position={myStart.position} icon={customIcon}>
+          <Popup>
+            <h3>{myStart.id}</h3>
+          </Popup>
+        </Marker>
+      }
+      {
+        myStart &&
+        <div> 
+          {
+            routes.map((route, index) => (
+              <MemoPolyline key={index} positions={route}/>
+            ))
+          } 
+        </div>
+      }
+      {
+        myStart &&
+        <div>
+          {destinations.map((point, index) => (
+            <Marker key={index} position={point.position} icon={customIconGray} eventHandlers={{
+              click: () => {
+                setMyStart(point);
+                              
+              },
+            }}>
+            </Marker>
+          ))}
+        </div>
+      }
+      {
+        myStart &&
+        <div>
+          {steps.map((point, index) => (
+            <Marker key={index} position={point.position} icon={customIconRed} eventHandlers={{
+              click: () => {
+                setMyStart(point);
+                              
+              },
+            }}>
+            </Marker>
+          ))}
+        </div>
+      }
+    </MapContainer>;
+    </div>
 }
 
 export default Map;
---
+++ web/src/components/slider.tsx
@@ -0,0 +1,17 @@
+  
+function Slider () {
+    <button>
+      TU VAS FONCTIONNER SINON!
+    </button>
+}
+
+/** <div>
+      <Slider
+        min={0}
+        max={24}
+        step={1}
+        value={5}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+      />
+    </div> */
