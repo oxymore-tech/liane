d9a9a1658b4242314aa13fae2c3591ac539bc6ee
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-02-24 10:30:40 +0100

    rechargind trips data

--- back/src/Liane/Liane.Test/Trips.cs
+++ back/src/Liane/Liane.Test/Trips.cs
@@ -8,6 +8,18 @@ namespace Liane.Test
         public static readonly Trip Mende_Florac = new Trip(ImmutableList.Create(LabeledPositions.Mende, LabeledPositions.LesBondons_Parking, LabeledPositions.Florac));
         public static readonly Trip Blajoux_Florac = new Trip(ImmutableList.Create(LabeledPositions.Blajoux_Parking, LabeledPositions.Florac));
         public static readonly Trip Blajoux_Mende = new Trip(ImmutableList.Create(LabeledPositions.Blajoux_Parking, LabeledPositions.Mende));
+        public static readonly Trip Mende_Florac_1 = new Trip(ImmutableList.Create(
+                LabeledPositions.Mende, 
+                LabeledPositions.Balsiege_Parking_Eglise,
+                LabeledPositions.Quezac_Parking,
+                LabeledPositions.Ispagnac_Parking,
+                LabeledPositions.Florac
+        ));
+        public static readonly Trip Mende_Florac_2 = new Trip(ImmutableList.Create(
+                LabeledPositions.Mende, 
+                LabeledPositions.SaintEtienneDuValdonnez_Parking,
+                LabeledPositions.Florac
+        ));
         public static readonly ImmutableList<Trip> AllTrips = ImmutableList.Create(
             Mende_Florac,
             Blajoux_Florac,
