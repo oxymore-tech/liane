407271d6ebde8c862981e4a60b627d39daf8ea96
Augustin Gjini <augustin.gjini@pm.me>
2021-04-21 16:03:27 +0200

    remove scenari

--- web/src/components/base/Modal.tsx
+++ web/src/components/base/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { displayService } from "../../api/display-service";
+import { displayService } from "@/api/display-service";
 import { TextInput } from "./TextInput";
 
 interface ModalProps {
@@ -11,53 +11,62 @@ interface ModalProps {
   onCancel?: () => any;
 }
 
-export function Modal({title, message, type, user, onConfirm, onCancel}: ModalProps) {
+export function Modal({ title, message, type, user, onConfirm, onCancel }: ModalProps) {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
 
-  function sendNotification() {
-    displayService.NotifyDriver(user, name, number);
+  async function sendNotification() {
+    await displayService.notifyDriver(user, name, number);
     onConfirm();
   }
-  
-  return <div className="fixed z-10 inset-0 overflow-y-auto">
-    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-        <div className="absolute inset-0 bg-gray-500 opacity-75"/>
-      </div>
 
-      <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-      <div
-        className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-        role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-          <div className="sm:flex sm:items-start">
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                {title}
-              </h3>
-              <div className="mt-2">
-                <form>
-                  <TextInput type="text" label="Prénom :" onChange={setName}></TextInput>
-                  <TextInput type="text" label="Numéro de téléphone :" onChange={setNumber}></TextInput>
-                </form>
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="absolute inset-0 bg-gray-500 opacity-75" />
+        </div>
+
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div
+          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                  {title}
+                </h3>
+                <div className="mt-2">
+                  <form>
+                    <TextInput type="text" label="Prénom :" onChange={setName} />
+                    <TextInput type="text" label="Numéro de téléphone :" onChange={setNumber} />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => sendNotification()}>
-            Confirmer
-          </button>
-          <button type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => onCancel()}>
-            Annuler
-          </button>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={() => sendNotification()}
+            >
+              Confirmer
+            </button>
+            <button
+              type="button"
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={() => onCancel()}
+            >
+              Annuler
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
 }
