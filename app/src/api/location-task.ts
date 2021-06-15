92cef3d84b1ea53aea8e9a8c68578945953d0c13
Elio <control.juin@gmail.com>
2021-06-15 17:10:41 +0200

    tweak(app): réflexion sur la localisation

--- app/src/api/location-task.ts
+++ app/src/api/location-task.ts
@@ -4,8 +4,44 @@ import * as TaskManager from "expo-task-manager";
 import { TaskManagerTaskBody } from "expo-task-manager";
 import { logLocation } from "@/api/client";
 import { storeLocation } from "@/api/location-storage";
+import { LocationPermissionLevel, UserLocation } from "@/api/index";
+import * as Device from "expo-device";
 
-const TASK_LOCATION_NAME = "TASK_LOCATION";
+const LOCATION_TASK_NAME = "location-task";
+const LOCATION_TASK_CHECK = "LOCATION_TASK_CHECK";
+const LOCATION_TASK_FOLLOW = "LOCATION_TASK_FOLLOW";
+
+/**
+ * Décrivons rapidemment l'algorithme que nous allons tenter de mettre en place :
+ * Cela fonctionne avec deux tâches afin d'obtenir un niveau de précision élevé tout en évitant de consommer beaucoup de
+ * batterie et de données :
+ *  -> Une première tâche vérifie toutes les 5 minutes si la personne s'est déplacée en sauvegardant la dernière donnée (en supprimant les précédentes ?)
+ *  -> Si la personne s'est déplacée : 500m ? 1km ? alors une autre tâche est lancée pour suivre la totalité du trajet
+ *  -> Cette seconde tâche s'arrête si la personne ne bouge plus pendant 5 minutes
+ * On pourrait envoyer les données une fois que le trajet est détecté comme terminé par l'application
+ *
+ * Données à ajouter :
+ * - niveau de permission
+ * - modèle du téléphone
+ */
+
+const isApple = Device.brand === "Apple";
+
+type LocationInformation = {
+  location: UserLocation;
+  permissionLevel: LocationPermissionLevel;
+  isApple: boolean;
+};
+
+const previousLocation = undefined;
+
+TaskManager.defineTask(LOCATION_TASK_CHECK, async ({ data, error }) => {
+
+});
+
+TaskManager.defineTask(LOCATION_TASK_FOLLOW, async (data: TaskManagerTaskBody) => {
+
+});
 
 export async function sendLocations(newLocations: LocationObject[] = []) {
   const locations = await storeLocation(newLocations);
@@ -25,7 +61,7 @@ export async function sendLocations(newLocations: LocationObject[] = []) {
  * Here we simply send the new locations data to the server.
  */
 export function listenLocationTask() {
-  TaskManager.defineTask(TASK_LOCATION_NAME, async (result: TaskManagerTaskBody) => {
+  TaskManager.defineTask(LOCATION_TASK_NAME, async (result: TaskManagerTaskBody) => {
     try {
       console.log("Nouvelle localisation : ", result);
       const { locations } = result.data as { locations:LocationObject[] };
@@ -39,18 +75,26 @@ export function listenLocationTask() {
 }
 
 /**
- * the function executed in the background which look at the
- * current position of the smartphone.
+ * Execute a task that will track the user position.
  */
-export async function registerLocationTask() {
+export async function startLocationTask(permissionLevel: LocationPermissionLevel) {
   try {
-    await Location.startLocationUpdatesAsync(TASK_LOCATION_NAME, {
-      // distanceInterval: 50,
-      // timeInterval : 30000
-      pausesUpdatesAutomatically: true,
-      activityType: Location.ActivityType.AutomotiveNavigation
-    });
-  } catch (err) {
-    console.log("Task Register failed : ", err);
+    if (permissionLevel === LocationPermissionLevel.ALWAYS) {
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        distanceInterval: 50,
+        // Android options
+        timeInterval: 60000,
+        // iOS options
+        pausesUpdatesAutomatically: true,
+        activityType: Location.ActivityType.AutomotiveNavigation
+
+      });
+    } else if (permissionLevel === LocationPermissionLevel.ACTIVE) {
+
+    } else {
+      console.log("Could not start tracking task as permission levels were not met.");
+    }
+  } catch (e) {
+    console.log(`Could not start tracking task : ${e}`);
   }
 }
