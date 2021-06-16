3aeae50154c9af77f8a243c787f85b56758666b1
AliceSenay <85169666+AliceSenay@users.noreply.github.com>
2021-06-16 17:34:15 +0200

    Essai d'autre style

--- app/src/components/HeaderMenu.tsx
+++ app/src/components/HeaderMenu.tsx
@@ -25,43 +25,52 @@ const HeaderMenu = ({ name }:HeaderMenuProps) => {
     }
   };
 
-  if (open) {
-    return (
-      <View>
-        <View style={tw("pt-6 pb-4 flex-row items-center bg-blue-400")}>
-          <AppButton
-            iconStyle={tw("text-2xl text-white")}
-            icon="menu"
-            onPress={chooseAction}
-          />
-          <AppText style={tw("text-3xl text-white text-center ")}>{name}</AppText>
-        </View>
-        <View>
-          <AppButton
-            buttonStyle={tw("bg-blue-500 rounded-full mt-5 ml-10 mr-10")}
-            title="Paramètres de géolocalisation"
-            onPress={() => { setAuthUser(); }}
-          />
-          <AppButton
-            buttonStyle={tw("bg-blue-500 rounded-full mt-5 ml-10 mr-10")}
-            title="Déconnexion"
-            onPress={() => { setLocationPermissionLevel(LocationPermissionLevel.NEVER); }}
-          />
-        </View>
-      </View>
-
-    );
-  }
-
   return (
-    <View style={tw("pt-6 pb-4 flex-row items-center bg-blue-400")}>
-      <AppButton
-        iconStyle={tw("text-2xl text-white")}
-        icon="menu"
-        onPress={chooseAction}
-      />
-      <AppText style={tw("text-3xl text-white text-center ")}>{name}</AppText>
+    <View>
+
+      {open
+        ? (
+          <View style={tw("pt-6 pb-4 flex-row items-center bg-blue-300")}>
+            <AppButton
+              buttonStyle={tw("bg-blue-300 ")}
+              iconStyle={tw("text-3xl font-bold text-white")}
+              icon="close-outline"
+              onPress={chooseAction}
+            />
+            <AppText style={tw("text-3xl text-white text-center ")}>{name}</AppText>
+          </View>
+        )
+        : (
+          <View style={tw("pt-6 pb-4 flex-row items-center bg-blue-500")}>
+            <AppButton
+              buttonStyle={tw("bg-blue-500 ")}
+              iconStyle={tw("text-3xl text-white")}
+              icon="menu"
+              onPress={chooseAction}
+            />
+            <AppText style={tw("text-3xl text-white text-center ")}>{name}</AppText>
+          </View>
+        )}
+
+      {open
+        ? (
+          <View style={tw("bg-blue-300 p-4")}>
+            <AppButton
+              buttonStyle={tw("bg-blue-500 ml-10 mr-10")}
+              title="Paramètres de géolocalisation"
+              onPress={() => { setLocationPermissionLevel(LocationPermissionLevel.NEVER); }}
+            />
+            <AppButton
+              buttonStyle={tw("bg-blue-500 mt-1 ml-10 mr-10")}
+              title="Déconnexion"
+              onPress={() => { setAuthUser(); }}
+            />
+          </View>
+        )
+        : null}
+
     </View>
+
   );
 
 };
