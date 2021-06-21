2bca418b0a2a5d317c88a1943e67d1e61a15572f
AliceSenay <85169666+AliceSenay@users.noreply.github.com>
2021-06-21 13:58:19 +0200

    tweak(web): Pages admin, affichage des rallying points

--- web/src/components/LianeMapAdmin.tsx
+++ web/src/components/LianeMapAdmin.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
-import { LatLng } from "@/api";
+import { CircleMarker, MapContainer, TileLayer, Tooltip } from "react-leaflet";
+import { LatLng, RallyingPoint, UserLocation } from "@/api";
+import { RallyingPointMarker } from "@/components/RallyingPointMarker";
+import { rallyingPointService } from "@/api/rallying-point-service";
 
 interface MapProps {
   className?: string;
   center: LatLng;
 }
+const Augustin = require("@/api/augustin.json");
 
 function LianeMapAdmin({ className, center }: MapProps) {
+
+  const [rallyingPoints, setRallyingPoints] = useState<RallyingPoint[]>([]);
+
+  useEffect(() => {
+    rallyingPointService.list(center.lat, center.lng)
+      .then((r) => {
+        setRallyingPoints(r);
+      });
+  }, [center]);
+
   return (
     <div>
       <MapContainer
@@ -24,6 +37,27 @@ function LianeMapAdmin({ className, center }: MapProps) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           zIndex={2}
         />
+        { /* displayRallyingPoints ? */ rallyingPoints.map(
+          (point, index) => (
+            <RallyingPointMarker
+              key={`rl_${index}`}
+              value={point}
+              onSelect={() => {}}
+            />
+          )
+        )/* : null */}
+        {Augustin.map((a:UserLocation, index:number) => (
+          <CircleMarker key={`a_${index}`} center={[a.latitude, a.longitude]} pathOptions={{ color: "red" }} radius={10}>
+            <Tooltip>
+              <p>
+                {new Intl.DateTimeFormat(
+                  "fr-FR", { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" }
+                ).format(new Date(a.timestamp))}
+              </p>
+              <p>{a.speed}</p>
+            </Tooltip>
+          </CircleMarker>
+        ))}
       </MapContainer>
     </div>
   );
