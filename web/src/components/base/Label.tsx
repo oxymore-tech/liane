98cb58c96bcba25a51866e123b8884b6f6157793
AliceSenay <85169666+AliceSenay@users.noreply.github.com>
2021-06-30 14:42:58 +0200

    fix(web): labels alignés pour les switch

--- web/src/components/base/Label.tsx
+++ web/src/components/base/Label.tsx
@@ -13,7 +13,7 @@ export function Label({ label, children, className = "", required, error }: Labe
     return <></>;
   }
   return (
-    <label className={`${className} font-semibold text-md ${error ? "text-red-700" : "text-gray-900"}`} title={required ? "Champ obligatoire" : undefined}>
+    <label className={`${className} font-semibold text-md flex items-center ${error ? "text-red-700" : "text-gray-900"}`} title={required ? "Champ obligatoire" : undefined}>
       {children || label}
       {required && <span className="ml-1 text-gray-400 text-xs">(obligatoire)</span>}
     </label>
