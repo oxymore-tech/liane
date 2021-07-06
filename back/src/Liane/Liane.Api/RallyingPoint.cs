6b38ea328913770b24d33b344278a15963a9b23d
Elio <control.juin@gmail.com>
2021-07-06 15:46:34 +0200

    tweak(back): proper typo for record

--- back/src/Liane/Liane.Api/RallyingPoint.cs
+++ back/src/Liane/Liane.Api/RallyingPoint.cs
@@ -2,5 +2,10 @@ using Liane.Api.Routing;
 
 namespace Liane.Api
 {
-    public sealed record RallyingPoint(string Id, LatLng Position, string Label, double? Distance = null);
+    public sealed record RallyingPoint(
+        string Id,
+        LatLng Position,
+        string Label,
+        double? Distance = null
+    );
 }
