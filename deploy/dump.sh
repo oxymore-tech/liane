4313e1039b387a2c0ef90c71d91027c22d07c502
Augustin Gjini <augustin.gjini@gmail.com>
2021-07-06 10:47:31 +0200

    feat(deploy): add dump.sh server utility

--- deploy/dump.sh
+++ deploy/dump.sh
@@ -2,6 +2,6 @@
 
 BASEDIR=$(dirname "$0")
 
-source .env
+source ${BASEDIR}/.env
 
 docker-compose -f ${BASEDIR}/docker-compose.yml -p liane exec mongo mongodump --archive --gzip -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} $*
