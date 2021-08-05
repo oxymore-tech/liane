51f2f16c805e7e5ff5a421d65debda3ebe95b5eb
Elio <control.juin@gmail.com>
2021-08-05 15:35:53 +0200

    tweak(web): enhance rp look

--- web/src/components/PopupMenuItem.tsx
+++ web/src/components/PopupMenuItem.tsx
@@ -8,7 +8,7 @@ interface PopupMenuItemProps {
 }
 
 export const PopupMenuItem = ({ text, selected, img, onSelect }:PopupMenuItemProps) => (
-  <button type="button" className="text-gray-900 hover:text-yellow-900 p-2 hover:bg-yellow-100 cursor-pointer select-none" onClick={onSelect}>
+  <button type="button" className="text-gray-900 hover:text-yellow-900 p-1 hover:bg-yellow-100 cursor-pointer select-none" onClick={onSelect}>
     <div className="flex items-center justify-left">
       <img className="h-6 mr-2" alt="" src={img} />
       <span className={`${selected ? "font-bold" : "font-normal"} text-gray-700`}>
