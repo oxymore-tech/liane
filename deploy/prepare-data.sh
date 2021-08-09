e827eafdedaf1701cfc2a39ab814ba62158018a6
Augustin Gjini <augustin.gjini@gmail.com>
2021-08-09 17:25:19 +0200

    add auvergne rhones alpes

--- deploy/prepare-data.sh
+++ deploy/prepare-data.sh
@@ -9,19 +9,26 @@ sudo mkdir -p ${OSM_DATA}
 function download() {
     wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
     wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
+    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf
+    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf
 }
 
 function merge() {
     if [[ ! -f ${OSM_DATA}/all.osm.pbf ]]
     then
-        docker run -t -v "${OSM_DATA}:/data" yagajs/osmosis osmosis --read-pbf /data/midi-pyrenees-latest.osm.pbf --read-pbf /data/languedoc-roussillon-latest.osm.pbf --merge --write-pbf /data/all.osm.pbf
+        docker run -t -v "${OSM_DATA}:/data" yagajs/osmosis osmosis \
+          --read-pbf /data/midi-pyrenees-latest.osm.pbf \
+          --read-pbf /data/languedoc-roussillon-latest.osm.pbf \
+          --read-pbf /data/auvergne-latest.osm.pbf \
+          --read-pbf /data/rhone-alpes-latest.osm.pbf \
+          --merge --write-pbf /data/all.osm.pbf
     fi;
 }
 
 function prepare() {
     local country=$1
 
-    docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
+    # docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
 
     docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
     docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
