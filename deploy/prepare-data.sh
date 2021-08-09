71669c7f0b550ade97f60d921b3b0cbb1e25271d
Augustin Gjini <augustin.gjini@gmail.com>
2021-08-09 18:10:40 +0200

    fix script for france

--- deploy/prepare-data.sh
+++ deploy/prepare-data.sh
@@ -29,7 +29,7 @@ function merge() {
 function prepare() {
     local country=$1
 
-    # docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
+    docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
 
     docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
     docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
