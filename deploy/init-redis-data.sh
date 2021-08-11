7e46b4ad14de606cd61eef533553820d66d8fe28
Elio <control.juin@gmail.com>
2021-08-11 15:40:51 +0200

    rem(back): useless init

--- deploy/init-redis-data.sh
+++ deploy/init-redis-data.sh
@@ -4,5 +4,4 @@ BASEDIR=$(dirname "$0")
 
 source .env
 
-docker exec -i liane_redis_1 redis-cli --pass "${REDIS_PASSWORD}" < "${BASEDIR}/rallying_points/rp.temprp.txt"
 docker exec -i liane_redis_1 redis-cli --pass "${REDIS_PASSWORD}" < "${BASEDIR}/administrators.txt"
