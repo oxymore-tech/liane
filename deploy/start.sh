001931cfb93c00c95824f1ba29c2a105479d5007
Augustin Gjini <augustin.gjini@gmail.com>
2022-07-01 11:25:54 +0200

    fix web build with eslint errors

--- deploy/start.sh
+++ deploy/start.sh
@@ -5,10 +5,10 @@ LIANE_HOME=$(cd "$(dirname "$0")/.." || exit;pwd)
 
 source "${LIANE_HOME}/deploy/utils.sh"
 
-PROJECT=$(getProject)
-
 createOsmNetwork
 
 docker-compose -f "${LIANE_HOME}/deploy/osm.yml" -p "osm" up -d
 
-docker-compose -f "${LIANE_HOME}/deploy/liane.yml" -p "${PROJECT}" up -d $*
+PROJECT=$(getProject) 
+
+docker-compose -f "${LIANE_HOME}/deploy/liane.yml" -p "${PROJECT}" -e "PROJECT=${PROJECT}" up -d $*
