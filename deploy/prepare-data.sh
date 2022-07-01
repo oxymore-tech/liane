eb44ce3885715917d450c9815a7836958f9f66db
Augustin Gjini <augustin.gjini@gmail.com>
2022-07-01 10:43:21 +0200

    feat(deploy): multi-env

--- deploy/prepare-data.sh
+++ deploy/prepare-data.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
-
-OSM_DATA=/data/liane/osm
-
-sudo mkdir -p ${OSM_DATA}
+sudo mkdir -p /data/osm
 
 function download() {
-    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france-latest.osm.pbf
-#    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
-#    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
-#    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf
-#    wget -P ${OSM_DATA} -N http://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf
+    local country=$1
+    wget -P /data/osm -N "http://download.geofabrik.de/europe/${country}.osm.pbf"
+#    wget -P /data/osm -N http://download.geofabrik.de/europe/france/midi-pyrenees-latest.osm.pbf
+#    wget -P /data/osm -N http://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.pbf
+#    wget -P /data/osm -N http://download.geofabrik.de/europe/france/auvergne-latest.osm.pbf
+#    wget -P /data/osm -N http://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf
 }
 
 function merge() {
-    if [[ ! -f ${OSM_DATA}/all.osm.pbf ]]
+    if [[ ! -f /data/osm/all.osm.pbf ]]
     then
-        docker run -t -v "${OSM_DATA}:/data" yagajs/osmosis osmosis \
+        docker run -t -v "/data/osm:/data" yagajs/osmosis osmosis \
           --read-pbf /data/midi-pyrenees-latest.osm.pbf \
           --read-pbf /data/languedoc-roussillon-latest.osm.pbf \
           --read-pbf /data/auvergne-latest.osm.pbf \
@@ -29,14 +26,14 @@ function merge() {
 function prepare() {
     local country=$1
 
-    docker run -t -v "${OSM_DATA}:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh /data/${country}.osm.pbf postgresdata 4:x
+    docker run -t -v "/data/osm:/data" docker.synergee.com/library/nominatim:3.5 sh /app/init.sh "/data/${country}.osm.pbf" nominatim 4:x
 
-    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${country}.osm.pbf
-    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-partition /data/${country}.osrm
-    docker run -t -v "${OSM_DATA}:/data" osrm/osrm-backend osrm-customize /data/${country}.osrm
+    docker run -t -v "/data/osm/osrm:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua "/data/${country}.osm.pbf"
+    docker run -t -v "/data/osm/osrm:/data" osrm/osrm-backend osrm-partition "/data/${country}.osrm"
+    docker run -t -v "/data/osm/osrm:/data" osrm/osrm-backend osrm-customize "/data/${country}.osrm"
 }
 
-download
+download france-latest
 
 #merge
 
