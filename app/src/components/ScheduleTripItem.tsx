eda5b5a579e82edce8550ac303a397ace1949c8b
Augustin Gjini <augustin.gjini@gmail.com>
2022-10-30 00:12:36 +0200

    fix tailwind config : remove tailwind-rn reference

--- app/src/components/ScheduleTripItem.tsx
+++ app/src/components/ScheduleTripItem.tsx
@@ -1,10 +1,9 @@
 import { Alert, TouchableOpacity, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { useTailwind } from "tailwind-rn";
 import { AppText } from "@/components/base/AppText";
 import { TripIntentMatch, RallyingPoint } from "@/api";
 import { Time } from "@/components/base/Time";
+import { AppIcon } from "@/components/base/AppIcon";
 
 interface ScheduleTripItemProps {
   tripIntentMatch: TripIntentMatch;
@@ -13,8 +12,6 @@ interface ScheduleTripItemProps {
 }
 
 const ScheduleTripItem = ({ tripIntentMatch, onDelete, onChat } : ScheduleTripItemProps) => {
-  const tw = useTailwind();
-
   const goToDetails = () => {
     onChat(tripIntentMatch);
   };
@@ -45,45 +42,45 @@ const ScheduleTripItem = ({ tripIntentMatch, onDelete, onChat } : ScheduleTripIt
 
   return (
     <TouchableOpacity
-      style={tw("flex flex-row bg-gray-200 h-20 mt-2 border-solid")}
+      className="flex flex-row bg-gray-200 h-20 mt-2 border-solid"
     >
 
-      <Ionicons
-        style={tw("flex-col self-center justify-around w-2/12")}
+      <AppIcon
+        className="flex-col self-center justify-around w-2/12"
         name="help-circle-outline"
         size={65}
       />
 
-      <View style={tw("flex-col justify-around w-2/12 mr-2 ")}>
+      <View className="flex-col justify-around w-2/12 mr-2 ">
         <Time value={tripIntent.goTime} />
 
-        <Ionicons name="chevron-down" style={tw("self-center opacity-0")} />
+        <AppIcon name="chevron-down" className="self-center opacity-0" />
 
         <Time value={tripIntent.returnTime} />
       </View>
 
-      <View style={tw("flex-col justify-around items-center w-5/12")}>
-        <AppText style={tw("flex-row")}>
+      <View className="flex-col justify-around items-center w-5/12">
+        <AppText className="flex-row">
           {from.label}
         </AppText>
 
-        <Ionicons name="chevron-down" style={tw("self-center")} />
+        <AppIcon name="chevron-down" className="self-center" />
 
-        <AppText style={tw("flex-row")}>
+        <AppText className="flex-row">
           {to.label}
         </AppText>
       </View>
 
-      <TouchableOpacity style={tw("flex-col justify-center items-center w-3/12 h-full pr-8")}>
-        <Ionicons
+      <TouchableOpacity className="flex-col justify-center items-center w-3/12 h-full pr-8">
+        <AppIcon
           name="chatbubbles-outline"
           size={35}
           onPress={goToDetails}
         />
       </TouchableOpacity>
 
-      <TouchableOpacity style={tw("absolute top-1 right-1 ")}>
-        <Ionicons
+      <TouchableOpacity className="absolute top-1 right-1 ">
+        <AppIcon
           name="close-outline"
           size={24}
           onPress={deleteIntent}
