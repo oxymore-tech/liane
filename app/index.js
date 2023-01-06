d6c5f62e1b052cabdb243cab4b1af8fffd90ebfe
Augustin Gjini <augustin.gjini@gmail.com>
2023-01-06 14:11:09 +0100

    fix(app)

--- app/index.js
+++ app/index.js
@@ -1,10 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent("liane", () => App);
 import 'react-native-gesture-handler' // Fix IOS issue, see https://github.com/software-mansion/react-native-gesture-handler/issues/320
