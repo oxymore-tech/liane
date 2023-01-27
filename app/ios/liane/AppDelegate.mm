3413c2ae214110cbcba9339abdcd0f2864bdbd84
Augustin Gjini <augustin.gjini@gmail.com>
2023-01-27 15:47:01 +0100

    fix(app): init firebase app in ios

--- app/ios/liane/AppDelegate.mm
+++ app/ios/liane/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
@@ -10,6 +11,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
