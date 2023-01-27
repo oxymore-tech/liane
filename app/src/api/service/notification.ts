f55db0e67c588cb6f010db3743e256ad63b69089
Augustin Gjini <augustin.gjini@gmail.com>
2023-01-27 18:44:46 +0100

    fix(app): display notification

--- app/src/api/service/notification.ts
+++ app/src/api/service/notification.ts
@@ -3,11 +3,33 @@ import notifee from "@notifee/react-native";
 
 export interface NotificationService {}
 
-function onMessageReceived(message: FirebaseMessagingTypes.RemoteMessage) {
-  return notifee.displayNotification(message);
+async function onMessageReceived(message: FirebaseMessagingTypes.RemoteMessage) {
+  if (!message.notification) {
+    return;
+  }
+  const { title, body } = message.notification;
+  return await notifee.displayNotification({
+    android: {
+      channelId: "default",
+      pressAction: {
+        id: "default"
+      }
+    },
+    title,
+    body
+  });
 }
 
-export function initializeNotification(): void {
+export async function initializeNotification() {
+  // Request permissions (required for iOS)
+  await notifee.requestPermission();
+
+  // Create a channel (required for Android)
+  await notifee.createChannel({
+    id: "default",
+    name: "Default Channel"
+  });
+
   messaging().onMessage(onMessageReceived);
   messaging().setBackgroundMessageHandler(onMessageReceived);
 }
