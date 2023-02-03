8f42c3edd16a5e270ed1e7a60dbe7be7f5a52f95
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-03 18:08:48 +0100

    feat(back): add debug notify route to send sms

--- back/src/Liane/Liane.Service/Internal/Notification/NotificationServiceImpl.cs
+++ back/src/Liane/Liane.Service/Internal/Notification/NotificationServiceImpl.cs
@@ -4,20 +4,30 @@ using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
 using Liane.Api.User;
 using Liane.Api.Util.Exception;
+using Liane.Service.Internal.User;
 using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace Liane.Service.Internal.Notification;
 
 public sealed class NotificationServiceImpl : INotificationService
 {
   private readonly IUserService userService;
+  private readonly TwilioSettings twilioSettings;
 
-  public NotificationServiceImpl(FirebaseSettings firebaseSettings, IUserService userService, ILogger<NotificationServiceImpl> logger)
+  public NotificationServiceImpl(FirebaseSettings firebaseSettings, IUserService userService, ILogger<NotificationServiceImpl> logger, TwilioSettings twilioSettings)
   {
     this.userService = userService;
+    this.twilioSettings = twilioSettings;
     if (firebaseSettings.ServiceAccountFile is null)
     {
       logger.LogWarning("Unable to init firebase because service account file is missing");
+    }
+
+    if (firebaseSettings.ServiceAccountFile is null)
+    {
+      return;
     }
 
     FirebaseApp.Create(new AppOptions { Credential = GoogleCredential.FromFile(firebaseSettings.ServiceAccountFile) });
@@ -31,6 +41,13 @@ public sealed class NotificationServiceImpl : INotificationService
       throw new ValidationException("pushToken", ValidationMessage.IsRequired);
     }
 
+    TwilioClient.Init(twilioSettings.Account, twilioSettings.Token);
+
+    await MessageResource.CreateAsync(
+      body: message,
+      from: twilioSettings.From.ToPhoneNumber(),
+      to: user.Phone.ToPhoneNumber()
+    );
     return await Send(user.PushToken, title, message);
   }
 
