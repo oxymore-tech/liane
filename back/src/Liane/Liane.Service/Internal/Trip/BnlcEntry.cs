98bab28706dd922e46796a8c7e1bdb45c2c164c8
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-09 12:21:54 +0100

    add bnlc to import aire de covoiturage

---
+++ back/src/Liane/Liane.Service/Internal/Trip/BnlcEntry.cs
@@ -0,0 +1,3 @@
+namespace Liane.Service.Internal.Trip;
+
+public sealed record BnlcEntry(string IdLieu, string NomLieu, string AdLieu, string ComLieu, string Insee, double XLong, double YLat);
--- back/src/Liane/Liane.Service/Internal/Trip/RallyingPointServiceImpl.cs
+++ back/src/Liane/Liane.Service/Internal/Trip/RallyingPointServiceImpl.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CsvHelper;
 using Liane.Api.Routing;
 using Liane.Api.Trip;
 using Liane.Api.Util.Exception;
@@ -25,127 +28,151 @@ internal sealed record OverpassTag(string Name);
 
 public sealed class RallyingPointServiceImpl : MongoCrudService<RallyingPoint>, IRallyingPointService
 {
-    private static readonly string[] AccentedChars =
+  private static readonly string[] AccentedChars =
+  {
+    "[aáÁàÀâÂäÄãÃåÅæÆ]",
+    "[cçÇ]",
+    "[eéÉèÈêÊëË]",
+    "[iíÍìÌîÎïÏ]",
+    "[nñÑ]",
+    "[oóÓòÒôÔöÖõÕøØœŒß]",
+    "[uúÚùÙûÛüÜ]"
+  };
+
+  private const int MaxRadius = 400_000;
+  private const int MaxRallyingPoint = 10;
+
+  private readonly ILogger<RallyingPointServiceImpl> logger;
+
+  public RallyingPointServiceImpl(IMongoDatabase mongo, ILogger<RallyingPointServiceImpl> logger)
+    : base(mongo)
+  {
+    this.logger = logger;
+  }
+
+  public async Task ImportCities()
+  {
+    await Mongo.GetCollection<RallyingPoint>()
+      .DeleteManyAsync(_ => true);
+
+    var assembly = typeof(RallyingPointServiceImpl).Assembly;
+
+    var rallyingPoints = await LoadCities(assembly);
+    await Mongo.GetCollection<RallyingPoint>()
+      .InsertManyAsync(rallyingPoints);
+    logger.LogInformation("Rallying points re-created with {Count} entries", rallyingPoints.Count);
+  }
+
+  public async Task<ImmutableList<RallyingPoint>> List(LatLng? pos, string? search)
+  {
+    var builder = Builders<RallyingPoint>.Filter;
+
+    var filter = FilterDefinition<RallyingPoint>.Empty;
+    if (pos.HasValue)
     {
-        "[aáÁàÀâÂäÄãÃåÅæÆ]",
-        "[cçÇ]",
-        "[eéÉèÈêÊëË]",
-        "[iíÍìÌîÎïÏ]",
-        "[nñÑ]",
-        "[oóÓòÒôÔöÖõÕøØœŒß]",
-        "[uúÚùÙûÛüÜ]"
-    };
-
-    private const int MaxRadius = 400_000;
-    private const int MaxRallyingPoint = 10;
-
-    private readonly ILogger<RallyingPointServiceImpl> logger;
-
-
-    public RallyingPointServiceImpl(IMongoDatabase mongo, ILogger<RallyingPointServiceImpl> logger)
-      : base(mongo)
-    {
-      this.logger = logger;
+      var point = GeoJson.Point(new GeoJson2DGeographicCoordinates(pos.Value.Lng, pos.Value.Lat));
+      filter &= builder.Near(x => x.Location, point, MaxRadius);
     }
 
-    public async Task ImportCities()
+    if (search != null)
     {
-        await Mongo.GetCollection<RallyingPoint>()
-            .DeleteManyAsync(_ => true);
-
-        var assembly = Assembly.GetEntryAssembly()!;
-
-        var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("cities.json"));
-        await using var file = assembly.GetManifestResourceStream(resourceName);
-        if (file is null)
-        {
-            throw new ResourceNotFoundException("Unable to find cities.json");
-        }
-
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        var data = await JsonSerializer.DeserializeAsync<OverpassData>(file, options);
-
-        if (data is not null)
-        {
-            var rallyingPoints = data.Elements.Select(e => new RallyingPoint($"city:{e.Id}", e.Tags.Name, new LatLng(e.Lat, e.Lon), true))
-                .ToList();
-            await Mongo.GetCollection<RallyingPoint>()
-                .InsertManyAsync(rallyingPoints);
-            logger.LogInformation("Rallying points re-created with {Count} entries", rallyingPoints.Count);
-        }
+      var regex = new Regex($".*{ToSearchPattern(search)}.*", RegexOptions.IgnoreCase);
+      filter &= builder.Regex(x => x.Label, new BsonRegularExpression(regex));
     }
 
-    public async Task<ImmutableList<RallyingPoint>> List(LatLng? pos, string? search)
+    return (await Mongo.GetCollection<RallyingPoint>()
+        .Find(filter)
+        .Limit(MaxRallyingPoint)
+        .ToCursorAsync())
+      .ToEnumerable()
+      .ToImmutableList();
+  }
+
+  public async Task<bool> Update(Ref<RallyingPoint> reference, RallyingPoint inputDto)
+  {
+    var res = await Mongo.GetCollection<RallyingPoint>()
+      .ReplaceOneAsync(
+        rp => rp.Id == reference.Id,
+        ToDb(inputDto, reference.Id)
+      );
+    return res.IsAcknowledged;
+  }
+
+  public async Task<RallyingPoint?> Snap(LatLng position)
+  {
+    const int radius = 100;
+    var builder = Builders<RallyingPoint>.Filter;
+    var point = GeoJson.Point(new GeoJson2DGeographicCoordinates(position.Lng, position.Lat));
+    var filter = builder.Near(x => x.Location, point, radius);
+
+    return await Mongo.GetCollection<RallyingPoint>()
+      .Find(filter)
+      .FirstOrDefaultAsync();
+  }
+
+  public async Task<ImmutableList<RallyingPoint>> Interpolate(ImmutableList<LatLng> locations)
+  {
+    var rallyingPoints = ImmutableList.CreateBuilder<RallyingPoint>();
+
+    foreach (var l in locations)
     {
-        var builder = Builders<RallyingPoint>.Filter;
+      var result = await Snap(l);
 
-        var filter = FilterDefinition<RallyingPoint>.Empty;
-        if (pos.HasValue)
-        {
-            var point = GeoJson.Point(new GeoJson2DGeographicCoordinates(pos.Value.Lng, pos.Value.Lat));
-            filter &= builder.Near(x => x.Location, point, MaxRadius);
-        }
+      if (result is null) continue;
 
-        if (search != null)
-        {
-            var regex = new Regex($".*{ToSearchPattern(search)}.*", RegexOptions.IgnoreCase);
-            filter &= builder.Regex(x => x.Label, new BsonRegularExpression(regex));
-        }
-
-        return (await Mongo.GetCollection<RallyingPoint>()
-                .Find(filter)
-                .Limit(MaxRallyingPoint)
-                .ToCursorAsync())
-            .ToEnumerable()
-            .ToImmutableList();
+      rallyingPoints.Add(result);
     }
 
-    public async Task<bool> Update(Ref<RallyingPoint> reference, RallyingPoint inputDto)
+    return rallyingPoints.Distinct().ToImmutableList();
+  }
+
+  private static string ToSearchPattern(string search)
+  {
+    search = Regex.Escape(search);
+    return AccentedChars.Aggregate(search, (current, accentedChar) => Regex.Replace(current, accentedChar, accentedChar));
+  }
+
+  protected override RallyingPoint ToDb(RallyingPoint inputDto, string id)
+  {
+    return inputDto with { Id = id };
+  }
+
+  private static async Task<ImmutableList<RallyingPoint>> LoadCities(Assembly assembly)
+  {
+    var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("cities.json"));
+    await using var file = assembly.GetManifestResourceStream(resourceName);
+    if (file is null)
     {
-       var res = await Mongo.GetCollection<RallyingPoint>()
-         .ReplaceOneAsync(
-              rp => rp.Id == reference.Id,
-              ToDb(inputDto, reference.Id)
-            );
-          return res.IsAcknowledged;
+      throw new ResourceNotFoundException("Unable to find cities.json");
     }
 
-    public async Task<RallyingPoint?> Snap(LatLng position)
-    {
-        const int radius = 100;
-        var builder = Builders<RallyingPoint>.Filter;
-        var point = GeoJson.Point(new GeoJson2DGeographicCoordinates(position.Lng, position.Lat));
-        var filter = builder.Near(x => x.Location, point, radius);
+    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    var data = await JsonSerializer.DeserializeAsync<OverpassData>(file, options);
 
-        return await Mongo.GetCollection<RallyingPoint>()
-            .Find(filter)
-            .FirstOrDefaultAsync();
+    if (data is not null)
+    {
+      return data.Elements.Select(e => new RallyingPoint($"city:{e.Id}", e.Tags.Name, new LatLng(e.Lat, e.Lon), true))
+        .ToImmutableList();
     }
 
-    public async Task<ImmutableList<RallyingPoint>> Interpolate(ImmutableList<LatLng> locations)
+    return ImmutableList<RallyingPoint>.Empty;
+  }
+
+  private static async Task<ImmutableList<RallyingPoint>> LoadCarpoolArea(Assembly assembly)
+  {
+    var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("bnlc.csv"));
+    await using var stream = assembly.GetManifestResourceStream(resourceName);
+    if (stream is null)
     {
-        var rallyingPoints = ImmutableList.CreateBuilder<RallyingPoint>();
-
-        foreach (var l in locations)
-        {
-            var result = await Snap(l);
-
-            if (result is null) continue;
-
-            rallyingPoints.Add(result);
-        }
-
-        return rallyingPoints.Distinct().ToImmutableList();
+      throw new ResourceNotFoundException("Unable to find cities.json");
     }
 
-    private static string ToSearchPattern(string search)
-    {
-        search = Regex.Escape(search);
-        return AccentedChars.Aggregate(search, (current, accentedChar) => Regex.Replace(current, accentedChar, accentedChar));
-    }
+    using var reader = new StreamReader(stream);
+    using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-    protected override RallyingPoint ToDb(RallyingPoint inputDto, string id)
-    {
-      return inputDto with { Id = id };
-    }
+    var entries = csvReader.GetRecords<BnlcEntry>();
+
+    return entries.Select(e => new RallyingPoint($"bnlc:{e.IdLieu}", e.NomLieu, new LatLng(e.YLat, e.XLong), true))
+      .ToImmutableList();
+  }
 }
