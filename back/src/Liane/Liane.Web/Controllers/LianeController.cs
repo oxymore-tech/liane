73991a7676651b6dbc38b30cbd439afff7faa5c0
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-10 10:56:51 +0100

    fix(back)

--- back/src/Liane/Liane.Web/Controllers/LianeController.cs
+++ back/src/Liane/Liane.Web/Controllers/LianeController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Liane.Web.Controllers;
 
-
 [Route("api/liane")]
 [ApiController]
 [RequiresAuth]
@@ -18,37 +17,35 @@ public sealed class LianeController : ControllerBase
   private readonly ICurrentContext currentContext;
 
 
-public LianeController(ILianeService lianeService, ICurrentContext currentContext)
-{
-  this.lianeService = lianeService;
-  this.currentContext = currentContext;
-}
+  public LianeController(ILianeService lianeService, ICurrentContext currentContext)
+  {
+    this.lianeService = lianeService;
+    this.currentContext = currentContext;
+  }
 
-    [HttpGet("{id}")]
-    [RequiresAccessLevel(ResourceAccessLevel.Member, typeof(Api.Trip.Liane))]
+  [HttpGet("{id}")]
+  [RequiresAccessLevel(ResourceAccessLevel.Member, typeof(Api.Trip.Liane))]
   public async Task<Api.Trip.Liane> Get([FromRoute] string id)
-    {
-      var current = currentContext.CurrentResource<Api.Trip.Liane>();
+  {
+    var current = currentContext.CurrentResource<Api.Trip.Liane>();
     return current ?? await lianeService.Get(id);
   }
 
   [HttpPost("")]
-  public Task<PaginatedResponse<Api.Trip.Liane, DatetimeCursor>> List([FromBody] Filter filter, Pagination<DatetimeCursor> pagination)
+  public Task<PaginatedResponse<Api.Trip.Liane, DatetimeCursor>> List([FromBody] Filter filter, [FromQuery] Pagination<DatetimeCursor> pagination)
   {
     return lianeService.List(filter, pagination);
   }
-  
+
   [HttpGet("")]
-  public Task<PaginatedResponse<Api.Trip.Liane, DatetimeCursor>> List(Pagination<DatetimeCursor> pagination)
-    {
+  public Task<PaginatedResponse<Api.Trip.Liane, DatetimeCursor>> List([FromQuery] Pagination<DatetimeCursor> pagination)
+  {
     return lianeService.ListForCurrentUser(pagination);
-    }
-    
-    [HttpPost("")]
-    public Task<Api.Trip.Liane> Create(LianeRequest lianeRequest)
-    {
-        return lianeService.Create(lianeRequest, currentContext.CurrentUser().Id);
-    }
-    
-    
+  }
+
+  [HttpPost("")]
+  public Task<Api.Trip.Liane> Create(LianeRequest lianeRequest)
+  {
+    return lianeService.Create(lianeRequest, currentContext.CurrentUser().Id);
+  }
 }
