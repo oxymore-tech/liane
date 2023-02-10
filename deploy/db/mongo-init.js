8e5484809fcb27443f6f65155e12e25b89d68c84
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-10 11:02:44 +0100

    fix(back)

--- deploy/db/mongo-init.js
+++ deploy/db/mongo-init.js
@@ -17,6 +17,8 @@ db.rallying_point.createIndex(
   }
 );
 
+db.chat_message.createIndex({CreatedAt: 1});
+
 db.trip_intent.createIndex({From: 1, To: 1, GoTime: 1, ReturnTime: 1, CreatedBy: 1}, {unique: true});
 
 db.user.createIndex({Phone: 1}, {unique: true});
