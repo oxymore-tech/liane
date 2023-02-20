04dfb24159ddaea6f457bdef03be4448d9bf506b
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-20 15:56:44 +0100

    fix(test): RoutingSerice is integration test

--- back/src/Liane/Liane.Test/ServiceLayerTest/RoutingServiceImplTest.cs
+++ back/src/Liane/Liane.Test/ServiceLayerTest/RoutingServiceImplTest.cs
@@ -7,23 +7,24 @@ using NUnit.Framework;
 
 namespace Liane.Test.ServiceLayerTest;
 
-public class RoutingServiceImplTest : BaseServiceLayerTest
+[TestFixture(Category = "Integration")]
+public sealed class RoutingServiceImplTest : BaseServiceLayerTest
 {
   private IRoutingService tested;
 
   protected override void InitService(IMongoDatabase db)
   {
-    tested =  ServiceProvider.GetService<IRoutingService>()!;
+    tested = ServiceProvider.GetService<IRoutingService>()!;
   }
 
   [Test]
   public async Task ReverseTripShouldReturnNull()
   {
     RouteSegment driver = (LabeledPositions.SaintEnimieParking, LabeledPositions.Mende);
-    var result = await tested.GetTrip(driver, new []{(RouteSegment)(LabeledPositions.Mende, LabeledPositions.SaintEnimieParking)});
+    var result = await tested.GetTrip(driver, new[] { (RouteSegment)(LabeledPositions.Mende, LabeledPositions.SaintEnimieParking) });
     Assert.Null(result);
-    
-    var result2 = await tested.GetTrip(driver, new []
+
+    var result2 = await tested.GetTrip(driver, new[]
     {
       (RouteSegment)(LabeledPositions.ChamperbouxEglise, LabeledPositions.SaintEnimieParking),
       (LabeledPositions.Mende, LabeledPositions.ChamperbouxEglise)
@@ -34,7 +35,6 @@ public class RoutingServiceImplTest : BaseServiceLayerTest
   [Test]
   public async Task TestTripComputation()
   {
-
     RouteSegment driver = (LabeledPositions.IspagnacParking, LabeledPositions.Mende);
     var segments = new[]
     {
@@ -43,19 +43,15 @@ public class RoutingServiceImplTest : BaseServiceLayerTest
       (LabeledPositions.IspagnacParking, LabeledPositions.BlajouxPelardon),
       (LabeledPositions.BlajouxParking, LabeledPositions.Mende),
     };
-    
+
     var result = await tested.GetTrip(driver, segments);
-    
+
     // Assert solution exists
     Assert.NotNull(result);
     // Assert start and end point are ok
-    Assert.AreEqual( driver.From.Id, result!.First().RallyingPoint.Id);
-    Assert.AreEqual( driver.To.Id, result!.Last().RallyingPoint.Id);
+    Assert.AreEqual(driver.From.Id, result!.First().RallyingPoint.Id);
+    Assert.AreEqual(driver.To.Id, result!.Last().RallyingPoint.Id);
     // Assert each "from" comes before its "to"
     Assert.True(segments.All(s => result!.First(w => w.RallyingPoint.Id == s.From.Id).Order < result!.First(w => w.RallyingPoint.Id == s.To.Id).Order));
-    
   }
-
-  
-  
 }
