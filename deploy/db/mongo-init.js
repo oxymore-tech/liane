fef9f2b4178abfe642a0892d84ab8df57142893c
Augustin Gjini <augustin.gjini@gmail.com>
2023-02-20 15:51:28 +0100

    feat(deploy): mongo liane.Geometry 2dsphere index

--- deploy/db/mongo-init.js
+++ deploy/db/mongo-init.js
@@ -24,3 +24,5 @@ db.chat_message.createIndex({CreatedAt: 1});
 db.trip_intent.createIndex({From: 1, To: 1, GoTime: 1, ReturnTime: 1, CreatedBy: 1}, {unique: true});
 
 db.user.createIndex({Phone: 1}, {unique: true});
+
+db.liane.createIndex({Geometry: "2dsphere"});
