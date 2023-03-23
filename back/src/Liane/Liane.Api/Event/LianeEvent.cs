194fae8ad19ce5abf029daab7266c14ec6af3d76
Augustin Gjini <augustin.gjini@gmail.com>
2023-03-23 17:23:44 +0100

    deploy

--- back/src/Liane/Liane.Api/Event/LianeEvent.cs
+++ back/src/Liane/Liane.Api/Event/LianeEvent.cs
@@ -4,9 +4,13 @@ using Liane.Api.Util.Ref;
 namespace Liane.Api.Event;
 
 [Union]
-public abstract record LianeEvent(Ref<Trip.Liane> Liane)
+public abstract record LianeEvent
 {
+  private LianeEvent()
+  {
+  }
 
+  public abstract Ref<Trip.Liane> Liane { get; init; }
 
   public sealed record JoinRequest(
     Ref<Trip.Liane> Liane,
@@ -15,7 +19,7 @@ public abstract record LianeEvent(Ref<Trip.Liane> Liane)
     int Seats,
     bool TakeReturnTrip,
     string Message
-  ) : LianeEvent(Liane);
+  ) : LianeEvent;
 
   public sealed record MemberAccepted(
     Ref<Trip.Liane> Liane,
@@ -24,10 +28,12 @@ public abstract record LianeEvent(Ref<Trip.Liane> Liane)
     Ref<RallyingPoint> To,
     int Seats,
     bool TakeReturnTrip
-  ) : LianeEvent(Liane);
+  ) : LianeEvent;
 
-  public sealed record MemberRejected(Ref<Trip.Liane> Liane, 
-    Ref<User.User> Member) : LianeEvent(Liane);
+  public sealed record MemberRejected(
+    Ref<Trip.Liane> Liane,
+    Ref<User.User> Member
+  ) : LianeEvent;
 
-  public sealed record MemberHasLeft(Ref<Trip.Liane> Liane) : LianeEvent(Liane);
+  public sealed record MemberHasLeft(Ref<Trip.Liane> Liane) : LianeEvent;
 }
