b9ec898a21094fc51db237494cfd9b1cca12bde8
Augustin Gjini <augustin.gjini@pm.me>
2023-05-25 17:53:15 +0200

    feat(back): do not send reminders to bots

--- back/src/Liane/Liane.Service/Internal/Trip/LianeStatusUpdate.cs
+++ back/src/Liane/Liane.Service/Internal/Trip/LianeStatusUpdate.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Liane.Api.Event;
 using Liane.Api.Trip;
+using Liane.Api.User;
 using Liane.Api.Util;
+using Liane.Api.Util.Ref;
 using Liane.Service.Internal.Mongo;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
@@ -17,13 +20,16 @@ public sealed class LianeStatusUpdate : CronJobService
   private readonly IMongoDatabase mongo;
   private readonly INotificationService notificationService;
   private readonly IRallyingPointService rallyingPointService;
+  private readonly IUserService userService;
 
-  public LianeStatusUpdate(ILogger<LianeStatusUpdate> logger, IMongoDatabase mongo, INotificationService notificationService, IRallyingPointService rallyingPointService) : base(logger, "* * * * *",
+  public LianeStatusUpdate(ILogger<LianeStatusUpdate> logger, IMongoDatabase mongo, INotificationService notificationService, IRallyingPointService rallyingPointService,
+    IUserService userService) : base(logger, "* * * * *",
     false)
   {
     this.mongo = mongo;
     this.notificationService = notificationService;
     this.rallyingPointService = rallyingPointService;
+    this.userService = userService;
   }
 
   protected override Task DoWork(CancellationToken cancellationToken) => Update(DateTime.UtcNow, TimeSpan.FromMinutes(5));
@@ -47,9 +53,7 @@ public sealed class LianeStatusUpdate : CronJobService
 
     foreach (var wayPoint in liane.WayPoints.Where(w => w.Eta > from && w.Eta <= to))
     {
-      var members = liane.Members.Where(m => m.From.Id == wayPoint.RallyingPoint.Id)
-        .Select(m => m.User)
-        .ToImmutableList();
+      var members = await GetRecipients(liane, wayPoint);
       if (members.IsEmpty)
       {
         continue;
@@ -61,5 +65,23 @@ public sealed class LianeStatusUpdate : CronJobService
         members,
         new Reminder(liane.Id, wayPoint.RallyingPoint, wayPoint.Eta));
     }
+  }
+
+  private async Task<ImmutableList<Ref<Api.User.User>>> GetRecipients(LianeDb liane, WayPointDb wayPoint)
+  {
+    var list = new List<Ref<Api.User.User>>();
+    foreach (var r in liane.Members.Where(m => m.From.Id == wayPoint.RallyingPoint.Id)
+               .Select(m => m.User))
+    {
+      var user = await userService.Get(r);
+      if ((user.Pseudo ?? "").StartsWith("Bot "))
+      {
+        continue;
+      }
+
+      list.Add(r);
+    }
+
+    return list.ToImmutableList();
   }
 }
