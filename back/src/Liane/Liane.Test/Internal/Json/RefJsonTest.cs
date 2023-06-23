ed590f3b15b6fd2272acb2d0ad9077bd9daf8ea5
Augustin Gjini <augustin.gjini@pm.me>
2023-06-23 13:25:36 +0200

    feat(back): update postgis on liane create, add/remove member

--- back/src/Liane/Liane.Test/Internal/Json/RefJsonTest.cs
+++ back/src/Liane/Liane.Test/Internal/Json/RefJsonTest.cs
@@ -1,9 +1,7 @@
 using System.Text.Json;
-using System.Text.Json.Serialization.Metadata;
 using Liane.Api.Routing;
 using Liane.Api.Trip;
 using Liane.Api.Util.Ref;
-using Liane.Service.Internal.Util;
 using Liane.Web.Internal.Json;
 using NUnit.Framework;
 
@@ -12,66 +10,61 @@ namespace Liane.Test.Internal.Json;
 [TestFixture]
 public sealed class RefJsonTest
 {
-    private readonly JsonSerializerOptions options = new() { 
-      PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
-      PropertyNameCaseInsensitive = true, 
-      Converters = { new RefJsonConverterFactory() }, 
-      TypeInfoResolver = new DefaultJsonTypeInfoResolver
-    {
-      Modifiers = { RefJsonStrategy.CreateRefResolutionModifier(new SnakeCaseNamingPolicy()) }
-    }};
+  private readonly JsonSerializerOptions options = JsonSerializerSettings.TestJsonOptions(false);
 
-    private record DummyDto(
-      Ref<RallyingPoint> rallyingPoint
-      );
-    private record DummyDtoWithResolvedRef(
-      [property: SerializeAsResolvedRef]
-      Ref<RallyingPoint> rallyingPoint
-    );
-    
-    [Test]
-    public void ShouldSerializeAnnotatedRefAsUnresolved()
-    {
-      var value = new DummyDto(LabeledPositions.Cocures);
-      var actual = JsonSerializer.Serialize(value, options);
-      Assert.AreEqual("{\"rallying_point\":\"Cocures_fakeId\"}", actual);
-    }
-    
-    [Test]
-    public void ShouldSerializeAnnotatedRefAsResolved()
-    {
-      var rallyingPoint = (Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true);
-      var value = new DummyDtoWithResolvedRef(rallyingPoint);
-      var actual = JsonSerializer.Serialize(value, options);
-      Assert.AreEqual("{\"rallying_point\":{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":1,\"address\":\"\",\"zip_code\":\"15000\",\"city\":\"\",\"place_count\":null,\"is_active\":true}}", actual);
-    }
+  private record DummyDto(Ref<RallyingPoint> RallyingPoint);
+
+  private record DummyDtoWithResolvedRef(
+    [property: SerializeAsResolvedRef] Ref<RallyingPoint> RallyingPoint
+  );
+
+  [Test]
+  public void ShouldSerializeAnnotatedRefAsUnresolved()
+  {
+    var value = new DummyDto(LabeledPositions.Cocures);
+    var actual = JsonSerializer.Serialize(value, options);
+    Assert.AreEqual("{\"rallyingPoint\":\"Cocures\"}", actual);
+  }
+
+  [Test]
+  public void ShouldSerializeAnnotatedRefAsResolved()
+  {
+    var rallyingPoint = (Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true);
+    var value = new DummyDtoWithResolvedRef(rallyingPoint);
+    var actual = JsonSerializer.Serialize(value, options);
+    Assert.AreEqual(
+      "{\"rallyingPoint\":{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":\"CarpoolArea\",\"address\":\"\",\"zipCode\":\"15000\",\"city\":\"\",\"placeCount\":null,\"isActive\":true}}",
+      actual);
+  }
 
 
-    [Test]
-    public void ShouldSerializeUnresolvedRef()
-    {
-        var actual = JsonSerializer.Serialize((Ref<RallyingPoint>)"XXX", options);
-        Assert.AreEqual("\"XXX\"", actual);
-    }
+  [Test]
+  public void ShouldSerializeUnresolvedRef()
+  {
+    var actual = JsonSerializer.Serialize((Ref<RallyingPoint>)"XXX", options);
+    Assert.AreEqual("\"XXX\"", actual);
+  }
 
-    [Test]
-    public void ShouldDeserializeUnresolvedRef()
-    {
-        var actual = JsonSerializer.Deserialize<Ref<RallyingPoint>>("\"AAA\"", options);
-        Assert.AreEqual((Ref<RallyingPoint>)"AAA", actual);
-    }
-    
-    [Test]
-    public void ShouldSerializeResolvedRef()
-    {
-        var actual = JsonSerializer.Serialize((Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true), options);
-        Assert.AreEqual("{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":1,\"address\":\"\",\"zip_code\":\"15000\",\"city\":\"\",\"place_count\":null,\"is_active\":true}", actual);
-    }
+  [Test]
+  public void ShouldDeserializeUnresolvedRef()
+  {
+    var actual = JsonSerializer.Deserialize<Ref<RallyingPoint>>("\"AAA\"", options);
+    Assert.AreEqual((Ref<RallyingPoint>)"AAA", actual);
+  }
 
-    [Test]
-    public void ShouldDeserializeResolvedRef()
-    {
-        var actual = JsonSerializer.Deserialize<Ref<RallyingPoint>>("{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":1,\"address\":\"\",\"zip_code\":\"15000\",\"city\":\"\",\"place_count\":null,\"is_active\":true}", options);
-        Assert.AreEqual((Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true), actual);
-    }
+  [Test]
+  public void ShouldSerializeResolvedRef()
+  {
+    var actual = JsonSerializer.Serialize((Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true), options);
+    Assert.AreEqual("{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":1,\"address\":\"\",\"zipCode\":\"15000\",\"city\":\"\",\"placeCount\":null,\"isActive\":true}",
+      actual);
+  }
+
+  [Test]
+  public void ShouldDeserializeResolvedRef()
+  {
+    var actual = JsonSerializer.Deserialize<Ref<RallyingPoint>>(
+      "{\"id\":\"33\",\"label\":\"Mende\",\"location\":{\"lat\":30,\"lng\":12},\"type\":\"CarpoolArea\",\"address\":\"\",\"zipCode\":\"15000\",\"city\":\"\",\"placeCount\":null,\"isActive\":true}", options);
+    Assert.AreEqual((Ref<RallyingPoint>)new RallyingPoint("33", "Mende", new LatLng(30.0, 12.0), LocationType.CarpoolArea, "", "15000", "", null, true), actual);
+  }
 }
