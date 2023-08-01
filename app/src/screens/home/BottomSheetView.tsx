e3809fece00aa4bb7c313a96fa7adb4869c4603c
Chloé L <c.laplagne8@gmail.com>
2023-08-01 10:22:29 +0200

    fix(app): return time on match item

--- app/src/screens/home/BottomSheetView.tsx
+++ app/src/screens/home/BottomSheetView.tsx
@@ -1,15 +1,15 @@
 import { Center, Column, Row } from "@/components/base/AppLayout";
 import React, { useContext, useEffect, useMemo, useState } from "react";
-import { ActivityIndicator, Platform, View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { AppColorPalettes, AppColors, WithAlpha } from "@/theme/colors";
-import { Exact, getPoint, RallyingPoint, RallyingPointLink, UnionUtils, UTCDateTime, WayPoint } from "@/api";
+import { Exact, getPoint, LianeMember, UnionUtils, UTCDateTime, WayPoint } from "@/api";
 import { AppPressableOverlay } from "@/components/base/AppPressable";
 import { TripSegmentView, TripViewStyles } from "@/components/trip/TripSegmentView";
 import { getTotalDuration, getTrip } from "@/components/trip/trip";
 import { AppText } from "@/components/base/AppText";
 import { filterHasFullTrip, HomeMapContext } from "@/screens/home/StateMachine";
 import { useActor } from "@xstate/react";
-import { AppContext } from "@/components/ContextProvider";
+
 import { TimeView } from "@/components/TimeView";
 import { AppRoundedButton } from "@/components/base/AppRoundedButton";
 import { useAppNavigation } from "@/api/navigation";
@@ -17,11 +17,6 @@ import { useAppNavigation } from "@/api/navigation";
 import { AppIcon } from "@/components/base/AppIcon";
 
 import { AppBottomSheetFlatList } from "@/components/base/AppBottomSheet";
-import { useQuery } from "react-query";
-
-import { Observable } from "rxjs";
-import { Feature } from "geojson";
-import { useObservable } from "@/util/hooks/subscription";
 import { AppTabs } from "@/components/base/AppTabs";
 import { SectionSeparator } from "@/components/Separator";
 import { UserPicture } from "@/components/UserPicture";
@@ -33,126 +28,6 @@ const EmptyResultView = (props: { message: string }) => (
 const ErrorView = (props: { message: string }) => (
   <AppText style={[{ paddingHorizontal: 24, paddingVertical: 8, alignSelf: "center", color: "red" }]}>{props.message}</AppText>
 );
-
-export const LianeDestinations = (props: {
-  pickup: RallyingPoint;
-  date: Date | undefined;
-  mapFeatureObservable: Observable<Feature[] | undefined>;
-}) => {
-  const { services } = useContext(AppContext);
-  const machine = useContext(HomeMapContext);
-  const { navigation } = useAppNavigation();
-
-  const features = useObservable(props.mapFeatureObservable, undefined);
-
-  const viewportLianes = features ? [...new Set<string>(features.map(f => f.properties!.id))] : undefined;
-  const {
-    data: results,
-    isLoading,
-    error
-  } = useQuery(["getNearestLinks", props.pickup.id, props.date?.toISOString(), viewportLianes?.sort().join(",")], async () => {
-    if (!viewportLianes) {
-      return undefined;
-    }
-
-    const res = await services.liane.pickupLinks(props.pickup.id!, viewportLianes);
-
-    const index = res.findIndex(p => p.pickup.id === props.pickup.id);
-    if (index < 0) {
-      return [];
-    }
-    return res[index].destinations;
-  });
-
-  if (error) {
-    /* @ts-ignore */
-    return <ErrorView message={error.message} />;
-  }
-  if (isLoading || !results) {
-    return <ActivityIndicator />;
-  }
-
-  return (
-    <Column spacing={8}>
-      {results.length === 0 && <EmptyResultView message={"Aucun trajet."} />}
-      {results.length === 0 && (
-        <Center style={{ paddingVertical: 8 }}>
-          <AppRoundedButton
-            backgroundColor={AppColors.orange}
-            text={"Ajouter une annonce"}
-            onPress={() => {
-              navigation.navigate("Publish", { initialValue: { from: props.pickup } });
-            }}
-          />
-        </Center>
-      )}
-      <AppBottomSheetFlatList
-        data={results}
-        keyExtractor={i => i.deposit.id!}
-        renderItem={({ item }) => {
-          return (
-            <LianeDestinationView
-              onPress={() => {
-                machine.send("UPDATE", { data: { from: props.pickup, to: item.deposit } });
-              }}
-              item={item}
-            />
-          );
-        }}
-      />
-    </Column>
-  );
-};
-
-const LianeDestinationView = (props: { onPress: () => void; item: RallyingPointLink }) => {
-  if (!props.item.deposit) {
-    console.warn("Empty deposit:", props.item);
-    return <View />;
-  }
-  return (
-    <AppPressableOverlay
-      foregroundColor={WithAlpha(AppColors.black, 0.1)}
-      style={{ paddingHorizontal: 24, paddingVertical: 8 }}
-      onPress={props.onPress}>
-      <Column spacing={4}>
-        <Row style={{ alignItems: "center" }} spacing={4}>
-          <AppIcon name={"pin"} color={AppColors.orange} size={18} />
-          <View
-            style={{
-              borderColor: AppColorPalettes.gray[400],
-              borderTopWidth: 1,
-              position: "absolute",
-              flex: 1,
-              left: 28,
-              width: 32
-            }}
-          />
-          <View style={{ width: 16 }} />
-          <AppIcon name={"arrow-right"} color={AppColorPalettes.gray[400]} />
-
-          <View style={{ justifyContent: "center" }}>
-            <AppText style={[TripViewStyles.mainWayPointLabel, TripViewStyles.toLabel, { maxWidth: "100%" }]}>{props.item.deposit.city}</AppText>
-          </View>
-        </Row>
-        <Row
-          style={{
-            justifyContent: "flex-start",
-            paddingLeft: 16,
-            marginLeft: 8,
-            borderLeftWidth: 2,
-            borderLeftColor: Platform.OS === "ios" ? AppColorPalettes.gray[400] : AppColors.orange,
-            borderStyle: Platform.OS === "ios" ? undefined : "dotted"
-          }}>
-          <AppText style={TripViewStyles.mainWayPointTime}>Départ à </AppText>
-          {props.item.hours.flatMap((h, index, array) => {
-            const time = <TimeView key={h} style={TripViewStyles.mainWayPointTime} value={h} />;
-            return array.length - 1 !== index ? [time, <AppText style={TripViewStyles.mainWayPointTime}>, </AppText>] : time;
-          })}
-        </Row>
-      </Column>
-    </AppPressableOverlay>
-  );
-};
 
 const formatSeatCount = (seatCount: number) => {
   let count = seatCount;
@@ -236,7 +111,7 @@ export const LianeMatchListView = ({ loading = false }: { loading?: boolean }) =
       const toPoint = getPoint(item, "deposit");
       const trip = getTrip(item.liane.departureTime, wayPoints, toPoint.id, fromPoint.id);
       const tripDuration = getTotalDuration(trip.wayPoints);
-      return { lianeMatch: item, lianeIsExactMatch, wayPoints, fromPoint, toPoint, trip, tripDuration };
+      return { lianeMatch: item, lianeIsExactMatch, wayPoints, fromPoint, toPoint, trip, tripDuration, returnTime: item.liane.returnTime };
     });
     const exact = matches.filter(i => {
       return i.fromPoint.id === state.context.filter.from!.id && i.toPoint.id === state.context.filter.to!.id;
@@ -267,7 +142,7 @@ export const LianeMatchListView = ({ loading = false }: { loading?: boolean }) =
   }
 
   const renderItem = ({ item }: { item: any }) => {
-    const driver = item.lianeMatch.liane.members.find(l => l.user.id === item.lianeMatch.liane.driver.user)!.user;
+    const driver = item.lianeMatch.liane.members.find((l: LianeMember) => l.user.id === item.lianeMatch.liane.driver.user)!.user;
     const duration = formatDuration(getTotalDuration(item.trip.wayPoints.slice(1)));
     return (
       <Column>
