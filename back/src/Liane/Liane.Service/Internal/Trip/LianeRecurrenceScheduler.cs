b865055b1c27cd8659a93ca5e6fde38eac1d068b
Augustin Gjini <augustin.gjini@pm.me>
2023-08-18 16:51:53 +0200

    fix(back)

--- back/src/Liane/Liane.Service/Internal/Trip/LianeRecurrenceScheduler.cs
+++ back/src/Liane/Liane.Service/Internal/Trip/LianeRecurrenceScheduler.cs
@@ -11,11 +11,12 @@ using MongoDB.Driver;
 
 namespace Liane.Service.Internal.Trip;
 
-public class LianeRecurrenceScheduler : CronJobService
+public sealed class LianeRecurrenceScheduler : CronJobService
 {
   private readonly IMongoDatabase mongo;
   private readonly ILianeService lianeService;
-  public LianeRecurrenceScheduler(ILogger logger, IMongoDatabase mongo, ILianeService lianeService) : base(logger, "0 0 0 * * ?", false)
+
+  public LianeRecurrenceScheduler(ILogger<LianeRecurrenceScheduler> logger, IMongoDatabase mongo, ILianeService lianeService) : base(logger, "0 0 0 * * ?", false)
   {
     this.mongo = mongo;
     this.lianeService = lianeService;
@@ -27,12 +28,12 @@ public class LianeRecurrenceScheduler : CronJobService
     var pattern = Enumerable.Repeat('.', 7).ToArray();
     pattern[(int)dayOfWeek - 1] = '1';
     var recurrences = await mongo.GetCollection<LianeRecurrence>().FindAsync(Builders<LianeRecurrence>.Filter.And(
-      Builders<LianeRecurrence>.Filter.Where(r => r.Active), 
+      Builders<LianeRecurrence>.Filter.Where(r => r.Active),
       Builders<LianeRecurrence>.Filter.Regex(r => r.Days, new BsonRegularExpression(new string(pattern)))
-      ));
+    ));
     foreach (var r in recurrences.ToEnumerable())
     {
-      await lianeService.CreateFromRecurrence( r, r.CreatedBy);
+      await lianeService.CreateFromRecurrence(r, r.CreatedBy);
     }
   }
 }
