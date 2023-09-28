bf6020fdadec9d12f1d45608b78cb0aaa70650d4
Chloe Laplagne <chloe.laplagne@etu.enseeiht.fr>
2023-09-28 09:10:06 +0200

    test(back): improve recurrence tests

--- back/src/Liane/Liane.Api/Trip/ILianeService.cs
+++ back/src/Liane/Liane.Api/Trip/ILianeService.cs
@@ -23,7 +23,7 @@ public interface ILianeService : ICrudEntityService<LianeRequest, Liane>
   Task<string> GetContact(Ref<Liane> id, Ref<User.User> requester, Ref<User.User> member);
   Task<Liane> UpdateDepartureTime(Ref<Liane> liane, DateTime departureTime);
   Task RemoveRecurrence(Ref<LianeRecurrence> recurrence);
-  Task<ImmutableList<Liane>> CreateFromRecurrence(Ref<LianeRecurrence> recurrence, Ref<Api.User.User>? owner = null);
+  Task<ImmutableList<Liane>> CreateFromRecurrence(Ref<LianeRecurrence> recurrence, Ref<Api.User.User>? owner = null, int daysAhead = 7);
   Task<Liane> GetForCurrentUser(Ref<Liane> liane);
   Task<FeatureCollection> GetGeolocationPings(Ref<Liane> liane);
   Task ForceSyncDatabase();
--- back/src/Liane/Liane.Service/Internal/Trip/LianeRecurrenceScheduler.cs
+++ back/src/Liane/Liane.Service/Internal/Trip/LianeRecurrenceScheduler.cs
@@ -1,37 +1,27 @@
-using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Liane.Api.Trip;
 using Liane.Api.Util;
-using Liane.Service.Internal.Mongo;
 using Microsoft.Extensions.Logging;
-using MongoDB.Bson;
-using MongoDB.Driver;
 
 namespace Liane.Service.Internal.Trip;
 
 public sealed class LianeRecurrenceScheduler : CronJobService
 {
-  private readonly IMongoDatabase mongo;
   private readonly ILianeService lianeService;
+  private readonly ILianeRecurrenceService lianeRecurrenceService;
 
-  public LianeRecurrenceScheduler(ILogger<LianeRecurrenceScheduler> logger, IMongoDatabase mongo, ILianeService lianeService) : base(logger, "0 0 * * *", false)
+  public LianeRecurrenceScheduler(ILogger<LianeRecurrenceScheduler> logger, ILianeService lianeService, ILianeRecurrenceService lianeRecurrenceService) : base(logger, "0 0 * * *", false)
   {
-    this.mongo = mongo;
     this.lianeService = lianeService;
+    this.lianeRecurrenceService = lianeRecurrenceService;
   }
 
   protected override async Task DoWork(CancellationToken cancellationToken)
   {
     Logger.Log(LogLevel.Debug, "Running recurrence scheduler...");
-    var now = DateTime.UtcNow;
-    var pattern = Enumerable.Repeat('.', 7).ToArray();
-    pattern[(int)now.DayOfWeek - 1] = '1';
-    var filter = Builders<LianeRecurrence>.Filter.Where(r => r.Active) & Builders<LianeRecurrence>.Filter.Regex(r => r.Days, new BsonRegularExpression(new string(pattern)));
-    var recurrences = await mongo.GetCollection<LianeRecurrence>()
-      .FindAsync(filter, cancellationToken: cancellationToken);
-    foreach (var r in recurrences.ToEnumerable())
+    var recurrences = await lianeRecurrenceService.GetUpdatableRecurrences();
+    foreach (var r in recurrences)
     {
       await lianeService.CreateFromRecurrence(r, r.CreatedBy);
     }
