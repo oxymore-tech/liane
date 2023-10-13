8bc57159d82b43c5e8e1ab72f0fcaf5ac70d4ab4
Augustin Gjini <augustin.gjini@pm.me>
2023-10-13 15:54:12 +0200

    feat(app): map ValidationError in app

--- app/src/api/exception.ts
+++ app/src/api/exception.ts
@@ -1,17 +1,40 @@
 export class ResourceNotFoundError extends Error {}
 
-export class ValidationError extends Error {
-  readonly errors: { [key: string]: string[] };
+export type ValidationCode = "required" | "wrong_format" | "unknown_value" | "too_short" | "already_member" | "liane_state_invalid" | string;
 
-  constructor(errors: { [key: string]: string[] } = {}) {
-    super("validation errors");
-    this.errors = errors;
+export type ValidationMessage = {
+  code: ValidationCode;
+  args: any;
+};
+
+function parseValidationMessage(raw: string): ValidationMessage {
+  const regex = /^(?<code>[a-z_]+)(?<args>([\s;]([a-z_]+:[^\s;]+))*)$/;
+  const match = regex.exec(raw);
+  if (!match?.groups) {
+    return { code: raw, args: {} };
   }
+
+  const { code, args } = match.groups;
+
+  return {
+    code,
+    args: Object.fromEntries(
+      args
+        .trim()
+        .split(";")
+        .map(a => a.split(":"))
+    )
+  };
 }
 
-export class BadRequest extends Error {
-  constructor(message: string) {
+export class ValidationError extends Error {
+  readonly errors: { [key: string]: ValidationMessage[] };
+  readonly validationMessage: ValidationMessage;
+
+  constructor(message: string, errors: { [key: string]: string[] } = {}) {
     super(message);
+    this.validationMessage = parseValidationMessage(message);
+    this.errors = Object.fromEntries(Object.entries(errors).map(([key, messages]) => [key, messages.map(parseValidationMessage)]));
   }
 }
 
