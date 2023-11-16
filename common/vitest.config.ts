4e85e1af6334b2b8342147e010419b05c315a7c9
Augustin Gjini <augustin.gjini@pm.me>
2023-11-16 14:21:40 +0100

    try to fix e2e report

--- common/vitest.config.ts
+++ common/vitest.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import { configDefaults } from "vitest/config";
+
 export default defineConfig({
   test: {
+    reporters: ["junit", "verbose"],
+    outputFile: {
+      junit: "./report/junit.xml"
+    },
     globals: true,
     include: ["tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     exclude: configDefaults.exclude
