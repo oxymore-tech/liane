7042e1c417fc8b6a713835b5f650faf21c6004f2
Augustin Gjini <augustin.gjini@pm.me>
2023-12-14 15:20:24 +0100

    fix(app): allow to publish a liane in the next minutes (4 min min delay before)

--- app/src/screens/publish/PublishScreen.tsx
+++ app/src/screens/publish/PublishScreen.tsx
@@ -278,7 +278,7 @@ const DateStepView = ({
   onRequestEdit
 }: StepProps<{ date: Date | undefined; recurrence: DayOfTheWeekFlag | null | undefined }>) => {
   const optionsRecurrentLiane = ["Date unique", "Trajet régulier"];
-  const initialMinDate = new Date(new Date().getTime() + 4 * 60000);
+  const initialMinDate = new Date(new Date().getTime() + 60000);
 
   const [date, setDate] = useState(initialMinDate);
   const [isRecurrent, setIsRecurrent] = useState(!!initialValue?.recurrence);
