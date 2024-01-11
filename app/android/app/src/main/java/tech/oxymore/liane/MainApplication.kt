a38fa5a0e3b9e12cc895e703603aeaee0e353d00
Augustin Gjini <augustin.gjini@pm.me>
2024-01-11 11:20:01 +0100

    rn 0.73.2

--- app/android/app/src/main/java/tech/oxymore/liane/MainApplication.kt
+++ app/android/app/src/main/java/tech/oxymore/liane/MainApplication.kt
@@ -15,26 +15,25 @@ import com.facebook.soloader.SoLoader
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> {
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          // add(MyReactNativePackage())
         }
-        
-        override fun getJSMainModuleName(): String = "index"
-        
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-        
-      }
-      
+
+      override fun getJSMainModuleName(): String = "index"
+
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+
   override val reactHost: ReactHost
     get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
-    
+
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
