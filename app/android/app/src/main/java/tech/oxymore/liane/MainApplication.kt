24745f44e0169503cacfc9be54813155ebd058e2
Augustin Gjini <augustin.gjini@pm.me>
2024-07-26 17:07:55 +0200

    chore(app): upgrade rn 0.73.4 0.74.3

--- app/android/app/src/main/java/tech/oxymore/liane/MainApplication.kt
+++ app/android/app/src/main/java/tech/oxymore/liane/MainApplication.kt
@@ -33,7 +33,7 @@ class MainApplication : Application(), ReactApplication {
     }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
@@ -42,6 +42,5 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
