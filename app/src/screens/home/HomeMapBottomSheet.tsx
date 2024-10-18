dd7fcd09783e35bf4ec9b8530dfeee80d8c32841
Freezzz <thibault.beaufort@gmail.com>
2024-10-18 09:12:08 +0200

    feat(liane): Handle in color

--- app/src/screens/home/HomeMapBottomSheet.tsx
+++ app/src/screens/home/HomeMapBottomSheet.tsx
@@ -39,6 +39,9 @@ export const HomeMapBottomSheetContainer = ({ colianes, isFetching, currentBound
       initialStop={0}
       backgroundStyle={{
         backgroundColor: lianeDetail ? AppColors.white : AppColors.gray100
+      }}
+      handlerColorStyle={{
+        backgroundColor: lianeDetail ? AppColors.white : AppColors.gray100
       }}>
       {lianeDetail ? (
         <View style={{ width: "100%", height: "100%", backgroundColor: AppColors.white }}>
