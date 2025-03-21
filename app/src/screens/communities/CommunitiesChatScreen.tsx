3729f2e0e556a496a01c296f5b491ec8a0d3ec30
Augustin Gjini <augustin.gjini@pm.me>
2025-03-21 11:21:45 +0100

    fix(app): Click sur une notification de message : mauvaise redirection #210

--- app/src/screens/communities/CommunitiesChatScreen.tsx
+++ app/src/screens/communities/CommunitiesChatScreen.tsx
@@ -117,7 +117,10 @@ export const CommunitiesChatScreen = () => {
     }
   }, [liane, paginationCursor, services.community]);
 
-  const me = useMemo(() => liane?.members.find(m => m.user.id === user!.id), [liane?.members, user]);
+  const me = useMemo(() => {
+    console.log("liane", liane);
+    return liane?.members.find(m => m.user.id === user!.id);
+  }, [liane?.members, user]);
   const name = me?.lianeRequest?.name ?? "";
   const [launching, setLaunching] = useState(false);
 
